9b633b49eb8d915746bc6fa3f171bbd01724934a
Jae <jaeladu1@gmail.com>
2021-03-31 18:21:00 -0300

    todos los test pasaron e index.ts funciona

--- models.ts
+++ models.ts
@@ -1,7 +1,38 @@
+import { readFileSync } from "fs";
+import { writeFileSync } from "jsonfile";
+
 class Contact {
-  id: number;
-  name: string;
+   id: number;
+   name: string;
+   // constructor(id?: number, name?: string) {
+   //    this.id = id;
+   //    this.name = name;
+   // }
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+   data: Contact[];
+
+   load() {
+      this.data = JSON.parse(
+         readFileSync(__dirname + "/contacts.json").toString()
+      );
+   }
+   getAll() {
+      return this.data;
+   }
+   addOne(contact: Contact) {
+      if (this.data) {
+         this.data.push(contact);
+      } else {
+         this.data = [contact];
+      }
+   }
+   save() {
+      writeFileSync(__dirname + "/contacts.json", this.data);
+   }
+   getOneById(id: number) {
+      return this.data.find((e) => e.id == id);
+   }
+}
+export { ContactsCollection, Contact };
