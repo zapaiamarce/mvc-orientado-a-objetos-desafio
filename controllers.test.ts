c261cd7b7e2179a1d236d7d045d5464ff039bdd3
Tobias <tobirastorza@gmail.com>
2021-03-30 23:09:13 -0300

    fixed programa

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,19 @@
 import test from "ava";
-import { ContactsController } from "./controllers";
+import { ContactsController, ContactsControllerOptions } from "./controllers";
 
 // test("Testeo el constructor del controller", (t) => {});
-
+test("Testeo el constructor del controller",(t) =>{
+    const test = new ContactsController();
+    t.is(test.contacts.data, test.contacts.getAll());
+})
 // test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions",(t) =>{
+    const controller = new ContactsController();
+    const options = controller.processOptions({
+        action: "get",
+        params: {id:2}
+    })
+
+    t.is(controller.contacts.getOneById(2), options)
+
+})
--- models.ts
+++ models.ts
@@ -1,7 +1,29 @@
+import * as jsonfile from "jsonfile"
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data:Contact[] = [];
+  load(){
+    const json = jsonfile.readFileSync("./contacts.json");
+    this.data = json;
+  }
+
+  getAll(){
+    return this.data;
+  }
+
+  addOne(contact:Contact){
+    this.data.push(contact);
+  }
+  save(){
+    jsonfile.writeFileSync("./contacts.json", this.data);
+  }
+  
+  getOneById(id){
+    return this.data.find(item => item.id == id);
+  }
+}
 export { ContactsCollection };
