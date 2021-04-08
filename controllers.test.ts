bd783c8d016be37d781d4501073a3e2416a2d00e
Monchi <conejobohemio@gmail.com>
2021-04-08 12:15:12 -0300

    Desafío completado

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,22 @@
 import test from "ava";
-import { ContactsController } from "./controllers";
+import { ContactsController, ContactsControllerOptions } from "./controllers";
+import { ContactsCollection } from "./models";
 
-// test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+  const contactController = new ContactsController();
+  contactController.contacts.load();
+  const mock = contactController.contacts.getOneById(3);
+  t.deepEqual(mock.name, "Mer");
+});
 
-// test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions", (t) => {
+  const contactController = new ContactsController();
+  contactController.contacts.load();
+
+  let options = new ContactsControllerOptions();
+  options.action = "get";
+  options.params = 2;
+
+  const contacto = contactController.processOptions(options);
+  t.deepEqual(contacto.name, "Paula");
+});
--- models.ts
+++ models.ts
@@ -1,7 +1,27 @@
+import * as jsonfile from "jsonfile";
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  contactos: Contact[] = [];
+  load() {
+    const json = jsonfile.readFileSync("./contacts.json");
+    this.contactos = json;
+  }
+  getAll() {
+    return this.contactos;
+  }
+  addOne(contact: Contact) {
+    this.contactos.push(contact);
+  }
+  save() {
+    jsonfile.writeFileSync("./contacts.json", this.contactos);
+  }
+  getOneById(id: number) {
+    var contactoEncontrado: any = this.contactos.find((i) => i.id == id);
+    return contactoEncontrado;
+  }
+}
+export { ContactsCollection, Contact };
