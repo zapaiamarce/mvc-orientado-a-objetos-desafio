da482233535ff287813cbc63e9e60c730ca8e7ea
Fernando de Row <ferderow@gmail.com>
2021-04-10 16:11:23 -0300

    aplicación terminada, pasa todo los test locales

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import * as minimist from "minimist";
 
 function parseaParams(argv): ContactsControllerOptions {
-  // parsear el argv usando https://www.npmjs.com/package/minimist
+  const parseado = minimist(argv);
   return {
-    action: null,
-    params: null,
+    action: parseado.action,
+    params: JSON.parse(parseado.params),
   };
 }
 
-function main() {}
+function main() {
+  const params = parseaParams(process.argv.slice(2));
+  const controller = new ContactsController();
+  const result = controller.processOptions(params);
+  console.log(result);
+}
 
 main();
--- models.ts
+++ models.ts
@@ -1,7 +1,43 @@
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number;
   name: string;
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+  load() {
+    const dbActual = jsonfile.readFileSync("./contacts.json");
+    this.data = dbActual;
+  }
+  getAll(): Object[] {
+    return this.data;
+  }
+  addOne(contact: Contact) {
+    this.data.push(contact);
+  }
+  save() {
+    const dbActualizada = this.data;
+    jsonfile.writeFileSync("./contacts.json", dbActualizada);
+  }
+  getOneById(id: number) {
+    const encontrado = this.data.find((contacto) => {
+      return contacto.id == id;
+    });
+    return encontrado;
+  }
+}
+
 export { ContactsCollection };
+
+/* Se podria refactorizar: 
+getOneById(id: number) {
+  return this.data.filter((c) => {
+    return c.id == id;
+  });
+}*/
