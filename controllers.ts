87b823623636767e43445f2e42657765513b9331
Franco <francomoya1022@gmail.com>
2021-04-19 23:23:21 -0300

    creé un model y controller de un listado de contactos

--- controllers.ts
+++ controllers.ts
@@ -7,7 +7,23 @@ export class ContactsControllerOptions {
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    const newContactCollection = new ContactsCollection();
+    this.contacts = newContactCollection;
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+    if (options.action === "get" && options.params.id) {
+      return this.contacts.getOneById(options.params.id);
+    }
+    if (options.action === "get") {
+      return this.contacts.getAll();
+    }
+    if (options.action === "save" && options.params) {
+      console.log(options);
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+    }
+  }
 }
 export { ContactsController };
