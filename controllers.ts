00cd634b647167837b44c4a5f37368424d050501
DenisMPar <paradadenismartin@gmail.com>
2021-04-20 12:58:09 -0300

    se realizaron los arreglos especificados en readme.md

--- controllers.ts
+++ controllers.ts
@@ -1,3 +1,4 @@
+
 import { ContactsCollection } from "./models";
 
 export class ContactsControllerOptions {
@@ -7,7 +8,36 @@ export class ContactsControllerOptions {
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    const contacts = new ContactsCollection()
+    contacts.load()
+    this.contacts = contacts
+  }
+  //invoca las funciones de models.ts dependiendo los valores de options
+  //si options.action == "get" => devuelve un contacto segun el id en options.params
+    // si options.params no tiene id, devuelve todos los contactos
+  //si options.action == "save" agrega como un contacto nuevo los valores de options.params
+  processOptions(options: ContactsControllerOptions) {
+    switch (options.action) {
+      case "get":
+        if (options.params.id) {
+          return this.contacts.getOneById(options.params.id)
+        }
+        else{
+          return this.contacts.getAll()
+        }
+        
+        case "save":
+         this.contacts.addOne(options.params)
+         this.contacts.save()
+         return "contacto agregado"
+    
+      default: return "opcion invalida"
+        
+    }
+  }
 }
+
+
+
 export { ContactsController };
--- index.ts
+++ index.ts
@@ -1,13 +1,21 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
 
+
 function parseaParams(argv): ContactsControllerOptions {
   // parsear el argv usando https://www.npmjs.com/package/minimist
-  return {
-    action: null,
-    params: null,
-  };
+   
+  return{
+    action: argv.action,
+    params: JSON.parse(argv.params)
+  }
+  
 }
 
-function main() {}
+function main() {
+    const argv = require('minimist')(process.argv.slice(2));
+    const controller = new ContactsController()
+
+   console.log(controller.processOptions(parseaParams(argv)))
+}
 
 main();
