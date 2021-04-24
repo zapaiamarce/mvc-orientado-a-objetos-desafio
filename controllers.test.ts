df9f2037ea3b6c33c9d614990eebdd846822e01c
Lucas Calvetti <lic.calvo099@gmail.com>
2021-04-24 02:13:56 -0300

    mejore los tests

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,42 +1,35 @@
 import test from "ava";
 import { ContactsController } from "./controllers";
-import { ContactsCollection } from "./models";
 import * as contactsObject from "./contacts.json";
 
-test("Testeo el constructor del controller, load()", (t) => {
+test("Testeo el constructor del controller", (t) => {
     let controller = new ContactsController(); 
-    t.deepEqual(controller.contacts.data, contactsObject) //testeo load()
-})
-test("Testeo el constructor del controller, contacts", (t) => {
-    let controller = new ContactsController(); 
-    let contactModel = new ContactsCollection; //testeo contacts
-    contactModel.load();
-    t.deepEqual(controller.contacts, contactModel)
+    t.deepEqual(controller.contacts.data, contactsObject)
 });
 
-test("Testeo el método processOptions si 'get' y id true", (t) => {
-    let controller1 = new ContactsController;
+test("Testeo el método processOptions; si 'get' y id: id:number", (t) => {
+    let controller = new ContactsController;
 
-    t.deepEqual(controller1.processOptions({
+    let expetcted = controller.processOptions({
         action: "get",
         params: {id: 1}
-    }), {id:1,"name":"Ana"});
-})
-test("Testeo el método processOptions si 'get' y id null", (t) => {
-    let controller2 = new ContactsController;
-
-    t.deepEqual(controller2.processOptions({
-        action: "get",
-        params: false
-    }), contactsObject);
-})
-test("Testeo el método processOptions si 'get' y contact", (t) => {
-    let controller3 = new ContactsController;
-
-    controller3.processOptions({
-        action: "save",
-        params: {id:8, name:"calvo"}
     });
-    controller3.contacts.load();
-    t.deepEqual(controller3.contacts.data[5],{id:8, name:"calvo"});
-})
+    t.deepEqual(expetcted, {id:1,"name":"Ana"});
+});
+test("Testeo el método processOptions; si 'get' y id:{}", (t) => {
+    let controller = new ContactsController;
+
+    let expetcted = controller.processOptions({
+        action: "get",
+        params: {}
+    });
+    t.deepEqual(expetcted, controller.contacts.getAll());
+});
+test("Testeo el método processOptions; si 'get' y contact", (t) => {
+    let controller = new ContactsController;
+    let expetcted = controller.processOptions({
+        action: "save",
+        params: {id:100, name:"calvo"}
+    });
+    t.deepEqual(controller.contacts.getOneById(100),{id:100, name:"calvo"});
+});
