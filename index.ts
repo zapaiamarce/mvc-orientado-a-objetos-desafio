176af3d8f2c48c6ee9696422dc399e57c1e37b4c
Alan Lipschutz <alanelipschutz@gmail.com>
2021-04-26 09:24:52 -0300

    arme models.ts, controllers.ts y sus test y el index.

--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import * as minimist from "minimist";
 
 function parseaParams(argv): ContactsControllerOptions {
   // parsear el argv usando https://www.npmjs.com/package/minimist
+  const resultado = minimist(argv);
   return {
-    action: null,
-    params: null,
+    action: resultado.action,
+    params: resultado.params,
   };
 }
 
-function main() {}
+function main() {
+  const salida = new ContactsController();
+  const salidaArgv = parseaParams(process.argv.slice(2));
+  const respuesta = salida.processOptions(salidaArgv);
+  console.log(respuesta);
+}
 
 main();
