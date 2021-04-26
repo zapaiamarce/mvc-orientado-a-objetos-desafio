5117277616fd71d544e43012104eaf16846b2a65
Gian <marattagian15@gmail.com>
2021-04-26 18:02:21 -0300

    MVC model implemented and currenty working

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,23 @@
 import test from "ava";
 import { ContactsController } from "./controllers";
+import * as contactObject from "./contacts.json"
 
-// test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+  const mockModel = new ContactsController;
+  t.deepEqual(contactObject, mockModel.contacts.getAll());
+});
 
-// test("Testeo el método processOptions", (t) => {});
+// test("Testeo el método processOptions", (t) => {
+//   const mockModel = new ContactsController;
+//   const option1 = { action: "get", params: {} }
+//   const option2 = { action: "get", params: {id: 3} }
+//   const option3 = { action: "save", params: {id: 7, name: "tete"}}
+//   const firstResult = mockModel.processOptions(option1)
+//   t.deepEqual(contactObject, firstResult)
+//   const secondResult = mockModel.processOptions(option2)
+//   const object2 = contactObject.find(i => i.id == 3)
+//   t.deepEqual(object2, secondResult)
+//   const thirdResult = mockModel.processOptions(option3)
+//   const trial = mockModel.contacts.getAll().includes({id: 7, name: "tete"})
+//   t.deepEqual(trial, thirdResult)
+// });
