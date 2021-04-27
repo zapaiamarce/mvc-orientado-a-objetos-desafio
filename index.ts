9cbe6ccda0967f29efee55d9f29f1083d0357ed2
Maximo <maximorossini2016@gmail.com>
2021-04-27 02:14:46 -0300

    mvc-orientado-a-objetos-completo

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import * as minimist from "minimist";
+import { ContactsCollection } from "./models";
 
 function parseaParams(argv): ContactsControllerOptions {
+  const parsearArgumentos = minimist(argv);
   // parsear el argv usando https://www.npmjs.com/package/minimist
   return {
     action: null,
@@ -8,6 +11,10 @@ function parseaParams(argv): ContactsControllerOptions {
   };
 }
 
-function main() {}
+function main() {
+  const parsearInput = parseaParams(process.argv.slice(2));
+  const controller = new ContactsController();
+  controller.processOptions(parsearInput)
+}
 
 main();
