69da2e81de1547b88796c6e34a43aebe2716c850
Santiago <romerogherdinasantiago@gmail.com>
2021-04-27 23:42:46 -0300

    Primer commit. Ya funciona todo teóricamente.

--- controllers.test.ts
+++ controllers.test.ts
@@ -2,5 +2,25 @@ import test from "ava";
 import { ContactsController } from "./controllers";
 
 // test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+    const unaPruebita = new ContactsController();
+
+    // Hago la prueba
+    t.is(unaPruebita.contacts.contactData, unaPruebita.contacts.getAll());
+});
 
 // test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions", (t) => {
+    const unaPruebita = new ContactsController();
+
+    // Hago el test
+    t.is(
+        unaPruebita.processOptions({
+            action: "get",
+            params: { id: 30 },
+        }),
+        unaPruebita.contacts.contactData[
+            unaPruebita.contacts.contactData.length - 1
+        ]
+    );
+});
--- index.ts
+++ index.ts
@@ -1,13 +1,37 @@
+import * as minimist from "minimist";
 import { ContactsController, ContactsControllerOptions } from "./controllers";
 
 function parseaParams(argv): ContactsControllerOptions {
-  // parsear el argv usando https://www.npmjs.com/package/minimist
-  return {
-    action: null,
-    params: null,
-  };
+    // parsear el argv usando https://www.npmjs.com/package/minimist
+    const finalArgs = minimist(argv);
+
+    // Auxiliar
+    let parametrosFinales = "";
+
+    if (finalArgs.params) {
+        parametrosFinales = JSON.parse(finalArgs.params);
+    }
+
+    return {
+        action: finalArgs.action,
+        params: parametrosFinales,
+    };
 }
 
-function main() {}
+// Función principal
+function main() {
+    // Primero, instancio
+    const controller = new ContactsController();
 
+    // Obtengo los argumentos
+    const argumentos = process.argv.slice(2);
+
+    // Los proceso con minimist
+    const argumentosParseados = parseaParams(argumentos);
+
+    // Invoco al método para procesar
+    console.log("\n", controller.processOptions(argumentosParseados));
+}
+
+// Ejecución del programa
 main();
