3bf55e2184bd938e2535dffc3e7e1190fca945dc
MarcosReuquen <marcosreuquendiaz@gmail.com>
2021-04-28 02:07:33 -0300

    Tests: ok. Desafio ok

--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import * as minimist from "minimist";
 
 function parseaParams(argv): ContactsControllerOptions {
   // parsear el argv usando https://www.npmjs.com/package/minimist
+  const objeto = minimist(argv);
+
   return {
-    action: null,
-    params: null,
+    action: objeto.action,
+    params: objeto.params,
   };
 }
 
-function main() {}
+function main() {
+  const entryParameters = parseaParams(process.argv.slice(2));
+  const contactControlerEntry = new ContactsController();
+  console.table(contactControlerEntry.processOptions(entryParameters));
+}
 
 main();
--- models.ts
+++ models.ts
@@ -1,7 +1,51 @@
+import * as fs from "fs";
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+  constructor() {
+    this.data = [];
+  }
+  load() {
+    //- debe tener un método load() que cargue la info desde el contacts.json y guardarla en data
+    this.data = JSON.parse(
+      fs.readFileSync(__dirname + "/contacts.json").toString()
+    );
+    return this.data;
+  }
+
+  getAll() {
+    //debe devolverme la collection completa
+    return this.data;
+  }
+
+  addOne(contact: Contact) {
+    //debe agregar un contacto a la lista
+    this.data.push(contact);
+    this.save();
+    return contact;
+  }
+
+  save() {
+    //debe escribir todo el objeto interno en el archivo contacts.json
+    return fs.writeFileSync(
+      __dirname + "/contacts.json",
+      JSON.stringify(this.data)
+    );
+  }
+
+  getOneById(id: number): Contact {
+    //debe devolver uno de los contactos por id
+    const data = this.load();
+    const contactoEncontrado = data.find((i) => {
+      return i.id == id;
+    });
+    return contactoEncontrado;
+  }
+}
+
 export { ContactsCollection };
