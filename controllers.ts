74027ac57f64584360e29fc1bae8effdab5d0a97
ArielGol <ariel00golubickas@gmail.com>
2021-04-29 15:36:18 -0300

    agregue un método getOneByName, los tests en modelo y controller y pasaron todos

--- controllers.ts
+++ controllers.ts
@@ -1,13 +1,42 @@
-import { ContactsCollection } from "./models";
+import { ContactsCollection,Contact} from "./models";
 
 export class ContactsControllerOptions {
-  action: "get" | "save";
-  params: any;
+  action: "get" | "save"|"getName";
+  params: Contact;
 }
+//- dentro del módulo **controllers.ts** crear la clase **ContactsController**
+//
+//- su constructor debe:
+// - instanciar **ContactsCollection** y guardarlo en una prop interna
+// - invocar al método load (para que se carge la data del archivo **contacts.json**)
+// - debe tener un método **processOptions** que reciba como parámetro un objeto del tipo **ContactsControllerOptions**
+// - dependiendo de las propiedades **action** y **params** debe interactuar con el modelo (ContactsController) e invocar distintos métodos
+// - en el caso de que el **action** sea "get" y el objeto **params** tenga un id, debe devolver un contacto en particular, si **id** no existe significa que debe devolver todos los contactos
+// - en el caso de que **action** sea "save" **params** es lo que se debe usar como contacto nuevo
+
+
+
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    this.contacts =new ContactsCollection();
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+    var resultado;
+    if(options.action== "get"&& options.params.id){
+      resultado =this.contacts.getOneById(options.params.id)
+    }else if (options.action =="get"){
+      resultado =this.contacts.getAll();
+    }else if(options.action =="getName"&& options.params.name){
+      resultado =this.contacts.getOneByName(options.params.name)
+    }
+    else if(options.action=="save"&&options.params){
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+    }
+    return resultado;
+  }
 }
 export { ContactsController };
--- models.ts
+++ models.ts
@@ -1,7 +1,50 @@
+import * as jsonfile from "jsonfile"; 
+
 class Contact {
   id: number;
   name: string;
 }
+//- dentro del módulo **models.ts** crear y exportar la clase **ContactsCollection**
 
-class ContactsCollection {}
-export { ContactsCollection };
+//  - la clase debe tener una propiedad interna (se puede llamar como quieran) donde se guarde el array con la data
+//  - debe tener un método **load()** que cargue la info desde el contacts.json y guardarla en **data**
+//  - getAll() debe devolverme la collection completa
+//  - addOne(contact) {} debe agregar un contacto a la lista
+//  - save debe escribir todo el objeto interno en el archivo **contacts.json**
+//  - getOneById(id) debe devolver uno de los contactos por id
+
+class ContactsCollection {
+data:Contact[]=[];
+load(){
+  const json = jsonfile.readFileSync("./contacts.json");
+  return this.data=json;
+}
+getAll(){
+  return this.data
+}
+addOne(contact:Contact){
+return this.data.push(contact);
+}
+save(){
+  return jsonfile.writeFileSync("./contacts.json",this.data);
+}
+getOneById(id){
+  const contactoEncontrado=this.data.find(function(contacto){
+    if(contacto.id == id){
+      return true;
+    }
+  });
+  return contactoEncontrado; 
+  };
+getOneByName(name){
+  const contactoEncontrado=this.data.find(function(contacto){
+    if(contacto.name==name){
+      return true;
+    }
+  });
+  return contactoEncontrado;
+};
+
+}
+
+export { ContactsCollection,Contact };
