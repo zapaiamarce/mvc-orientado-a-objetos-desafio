59a909256c48f73934ef9d891283fecce3c0cabb
Ezegrandia <ezequielgrandia@gmail.com>
2021-05-02 01:50:27 -0300

    Desafio Completo

--- controllers.ts
+++ controllers.ts
@@ -1,13 +1,29 @@
 import { ContactsCollection } from "./models";
 
-export class ContactsControllerOptions {
+class ContactsControllerOptions {
   action: "get" | "save";
   params: any;
 }
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+    var resultado;
+    if (options.action == "get" && options.params.id) {
+      resultado = this.contacts.getOneById(options.params.id);
+    } else if (options.action == "get") {
+      resultado = this.contacts.getAll();
+    } else if (options.action == "save") {
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+      console.log("Nuevo contacto agregado:", options.params);
+      return true;
+    }
+    return resultado;
+  }
 }
-export { ContactsController };
+export { ContactsController, ContactsControllerOptions };
