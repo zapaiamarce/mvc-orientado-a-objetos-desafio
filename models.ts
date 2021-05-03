d47e01a52f82ea821ce6ee7cd04e2bc1823a709d
Rafa Lynch <rafa_lynch@hotmail.com>
2021-05-03 17:26:57 -0300

    first commit. Models.ts is done.

--- models.ts
+++ models.ts
@@ -1,7 +1,33 @@
+import * as contactsJson from './contacts.json';
+import * as fs from 'fs';
+
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  contacts: Contact[] = []
+  load(){
+    contactsJson.map(
+      c => this.contacts.push(c)
+    )
+  }
+  getAll(){
+    return this.contacts
+  }
+  addOne(contact:Contact){
+    this.contacts.push(contact)
+  }
+  save(){
+    fs.writeFileSync("./contacts.json", JSON.stringify(this.contacts));
+  }
+  getOneById(id){
+    return this.contacts.find(
+      c => c.id == id
+    )
+  }
+}
+
 export { ContactsCollection };
