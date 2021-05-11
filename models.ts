4d2ae7a2f2636d339dcc38a8ac60f05011d87406
lautaro <lautarogomez68@gmail.com>
2021-05-10 21:46:51 -0300

    completado

--- models.ts
+++ models.ts
@@ -1,7 +1,31 @@
+import * as jsonfile from "jsonfile";
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+  load(){
+    const json = jsonfile.readFileSync("./contacts.json");
+    this.data = json;
+  }
+  getAll(){
+    return this.data;
+  }
+  addOne(contact){
+    this.data.push(contact);
+  }
+  save(){
+    jsonfile.writeFileSync("./contacts.json", this.data);
+  }
+  getOneById(id){
+    const idEncontrado = this.data.find((c)=>{
+      if (c.id == id){
+        return true;
+      }
+    })
+    return idEncontrado;
+  }
+}
 export { ContactsCollection };
