330216a3105188d42804b7dd7d09889ea503826e
EzequielVilla <ezequiel.n.villa@gmail.com>
2021-05-12 14:52:47 -0300

    Desafio de la clase 10

--- index.ts
+++ index.ts
@@ -1,13 +1,28 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
 
-function parseaParams(argv): ContactsControllerOptions {
+function parseaParams(argv)/*: ContactsControllerOptions*/ {
   // parsear el argv usando https://www.npmjs.com/package/minimist
+  var dato = require('minimist')(argv.slice(2));
+  if(dato.params == true){
+    dato.params = {id: dato.id, name:dato.name};
+  }
+
   return {
-    action: null,
-    params: null,
+    action: dato.action,
+    params: dato.params,
   };
 }
 
-function main() {}
+function main() {
+const datoParseado = parseaParams(process.argv);
+const controller = new ContactsController();
+const options = new ContactsControllerOptions();
+options.action = datoParseado.action;
+options.params = datoParseado.params;
+controller.processOptions(options);
+
+
+console.log(controller.processOptions(options));
+}
 
 main();
