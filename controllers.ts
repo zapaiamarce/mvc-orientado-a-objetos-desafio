1b1f78e7680e50bc6824d26b7e2053a488506b94
Franco Brandan <brandanfranco0@gmail.com>
2021-05-14 11:09:02 +0200

    desafio capitulo 10 modulo 2

--- controllers.ts
+++ controllers.ts
@@ -7,7 +7,29 @@ export class ContactsControllerOptions {
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    const contactos = new ContactsCollection()
+    this.contacts = contactos
+    contactos.load()
+  }
+  processOptions(options: ContactsControllerOptions){
+
+    let resp : any
+
+    if(options.action === "get" && options.params.id){
+            resp = this.contacts.getOneById(options.params.id)
+    }else if(options.action === "save" && options.params.id){
+
+          
+                this.contacts.addOne(options.params)
+                this.contacts.save()
+                     
+    }else if(options.action === "get" &&!options.params){
+            resp = this.contacts.getAll()
+    }
+
+    return resp
+
+  }
 }
 export { ContactsController };
