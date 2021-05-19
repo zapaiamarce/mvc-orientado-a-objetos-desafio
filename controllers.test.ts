580d089a6d7965fae8b77b72fe6d51a80ef74e28
Matias Toledo <toledo.nicolas.matias@gmail.com>
2021-05-19 12:55:47 -0300

    Desafio terminado

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,29 @@
 import test from "ava";
-import { ContactsController } from "./controllers";
+import { ContactsController, ContactsControllerOptions } from "./controllers";
+import { ContactsCollection } from "./models";
 
 // test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+  const controller = new ContactsController();
+  const prueba = controller.contacts;
+  const collection = new ContactsCollection();
+  collection.load();
+  const contacts = collection.data;
+  t.deepEqual(prueba.data, contacts);
+});
 
 // test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions", (t) => {
+  const controllerOptions = new ContactsControllerOptions();
+  controllerOptions.action = "get";
+  controllerOptions.params = { id: 1 };
+
+  const contactsController = new ContactsController();
+  const process = contactsController.processOptions(controllerOptions);
+
+  const contactsCollection = new ContactsCollection();
+  contactsCollection.load();
+  const contacts = contactsCollection.getOneById(1);
+
+  t.deepEqual(process, contacts);
+});
