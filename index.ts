9c94e72e9a2c1307903792a77f43fdeb5652d2d1
Junior Vitteri <vitteridev@gmail.com>
2021-05-24 07:19:31 -0300

    añadiendo metodos

--- index.ts
+++ index.ts
@@ -1,13 +1,21 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import { ContactsCollection } from "./models";
+import * as minimist from "minimist";
 
 function parseaParams(argv): ContactsControllerOptions {
   // parsear el argv usando https://www.npmjs.com/package/minimist
+  const parseando = minimist(argv.slice(2));
+
   return {
-    action: null,
-    params: null,
+    action: parseando.action,
+    params: JSON.parse(parseando.params),
   };
 }
 
-function main() {}
-
+function main() {
+  const controller = new ContactsController();
+  const params = parseaParams(process.argv);
+  const resultad = controller.processOptions(params);
+  console.log(resultad);
+}
 main();
