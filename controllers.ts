8d723a5f7cc8caf3048346df9cc62808fc57f83a
Pablo Martins De Maltoz <martins2020@live.com>
2021-05-26 14:06:30 -0300

    DesafioCompletado

--- controllers.ts
+++ controllers.ts
@@ -1,13 +1,34 @@
+import { openSync } from "node:fs";
 import { ContactsCollection } from "./models";
 
-export class ContactsControllerOptions {
+class ContactsControllerOptions {
+  constructor(accion: "get" | "save", parametro: any) {
+    this.action = accion;
+    this.params = parametro;
+  }
   action: "get" | "save";
   params: any;
 }
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+
+  processOptions(options: ContactsControllerOptions) {
+    let resultado;
+
+    if (options.action == "get" && options.params.id) {
+      resultado = this.contacts.getOneById(options.params.id);
+    } else if (options.action == "get") {
+      resultado = this.contacts.getAll();
+    } else if (options.action == "save" && options.params) {
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+    }
+    return resultado;
+  }
 }
-export { ContactsController };
+export { ContactsController, ContactsControllerOptions };
--- index.ts
+++ index.ts
@@ -1,13 +1,27 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import * as minimist from "minimist";
+import { parse } from "node:path";
+import { argv } from "node:process";
+import { stringify } from "node:querystring";
+import { Contact } from "./models";
 
 function parseaParams(argv): ContactsControllerOptions {
   // parsear el argv usando https://www.npmjs.com/package/minimist
-  return {
-    action: null,
-    params: null,
+  const resultado = minimist(argv);
+  const resuParams = resultado.params;
+  const jsonParseDeParams = JSON.parse(resuParams);
+  const elObjeto = {
+    action: resultado.action,
+    params: jsonParseDeParams,
   };
+  return elObjeto;
 }
 
-function main() {}
+function main() {
+  const controller = new ContactsController();
+  const ParaPasarAlController = parseaParams(process.argv.slice(2));
+  const ejecutor = controller.processOptions(ParaPasarAlController);
+  console.log(ejecutor);
+}
 
 main();
