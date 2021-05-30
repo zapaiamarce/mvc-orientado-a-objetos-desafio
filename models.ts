a432dfd9260f4b3e02a59ebd3588340f2bb1c4ee
Lorenzo <lorenzoburgi@gmail.com>
2021-05-30 18:06:29 -0300

    Desafio finalizado

--- models.ts
+++ models.ts
@@ -1,7 +1,35 @@
+import * as fs from "fs";
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+
+  load() {
+    const require = fs.readFileSync("./contacts.json").toString();
+    const parseo = JSON.parse(require);
+    this.data = parseo;
+  }
+
+  getAll() {
+    return this.data;
+  }
+
+  addOne(contact: Contact) {
+    this.data.push(contact);
+  }
+
+  save() {
+    jsonfile.writeFileSync("./contacts.json", this.data);
+  }
+
+  getOneById(id: number) {
+    return this.data.find((i) => i.id == id);
+  }
+}
+
 export { ContactsCollection };
