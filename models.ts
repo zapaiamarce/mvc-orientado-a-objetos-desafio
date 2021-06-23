a4a4de180a97a5e6d491a2451d028519cd214c23
Misael <dmisael2000@gmail.com>
2021-06-23 12:31:47 -0300

    cambio en controller.test y class ContactsController

--- models.ts
+++ models.ts
@@ -1,7 +1,32 @@
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  arrayData: Contact[] = [];
+  load() {
+    const readJson = jsonfile.readFileSync("./contacts.json");
+    this.arrayData = readJson;
+  }
+
+  getAll() {
+    return this.arrayData;
+  }
+  addOne(contact: Contact) {
+    this.arrayData.push(contact);
+  }
+  save() {
+    jsonfile.writeFileSync("./contacts.json", this.arrayData);
+  }
+  getOneById(id) {
+    const foundId = this.arrayData.find((e) => {
+      return e.id == id;
+    });
+    return foundId;
+  }
+}
+
+export { ContactsCollection, Contact };
