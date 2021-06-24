49987bc688a62f8d9c28d4d9b0688ca963e79e9b
heberjbaez <heberjbaez@gmail.com>
2021-06-23 22:25:43 -0300

    commit test desafio pero con la resolucion de marce

--- models.ts
+++ models.ts
@@ -1,7 +1,35 @@
+import * as jsonfile from "jsonfile";
+const json = jsonfile.readFileSync("./contacts.json");
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  data: Contact[] = []; //data es una collección de Contact
+  load() {
+    this.data = json;
+  }
+
+  getAll() {
+    return this.data
+  }
+
+  addOne(contact: Contact) {
+    this.data.push(contact)
+  }
+
+  save() {
+    jsonfile.writeFileSync("./contacts.json", this.data)
+  }
+
+  getOneById(id) {
+    return this.data.find((contacto) => {
+      if (contacto.id == id) {
+        return true;
+      }
+    })
+  }
+}
+export { ContactsCollection, Contact };
