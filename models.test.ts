6ea733e26eb3630477a6b71d5868d91c14e011b0
Camila Vazquez <camilavazquezvazquez2014@outlook.com>
2021-06-24 22:44:43 -0300

    agregue cosas nuevas

--- models.test.ts
+++ models.test.ts
@@ -4,20 +4,21 @@ import * as contactsObject from "./contacts.json";
 import * as jsonfile from "jsonfile";
 
 test("Testeo el load del modelo", (t) => {
-  const model = new ContactsCollection();
-  model.load();
-  t.deepEqual(contactsObject, model.getAll());
+  const newModel = new ContactsCollection();
+  newModel.load();
+  t.deepEqual(contactsObject, newModel.getAll());
 });
 
 test("Testeo el addOne del modelo", (t) => {
-  const model = new ContactsCollection();
+  const newContactsCollection = new ContactsCollection();
   const mockContact = {
     id: 30,
     name: "Marce",
   };
-  model.addOne(mockContact);
-  t.deepEqual(model.getAll(), [mockContact]);
+  newContactsCollection.addOne(mockContact);
+  t.deepEqual(newContactsCollection.getAll(), [mockContact]);
 });
+
 
 test("Testeo el save del modelo", (t) => {
   const model = new ContactsCollection();
@@ -26,19 +27,20 @@ test("Testeo el save del modelo", (t) => {
     id: 30,
     name: "Marce",
   };
-  model.addOne(mockContact);
-  model.save();
-  const fileContent = jsonfile.readFileSync("./contacts.json");
-  t.deepEqual(fileContent, model.getAll());
+model.addOne(mockContact);
+model.save();
+const fileContent = jsonfile.readFileSync("./contacts.json"); 
+t.deepEqual(fileContent, model.getAll());
 });
 
-test("Testeo el getOneById del modelo", (t) => {
+test("Testo el getOneById del modelo", (t) => {
   const model = new ContactsCollection();
   const mockContact = {
     id: 31,
     name: "Marce",
   };
-  model.addOne(mockContact);
-  const one = model.getOneById(31);
-  t.deepEqual(one, mockContact);
+model.addOne(mockContact);
+model.getOneById(31);
+const one = model.getOneById(31);
+t.deepEqual(one, mockContact);
 });
--- models.ts
+++ models.ts
@@ -1,7 +1,34 @@
+import * as jsonfile from "jsonfile";
+import * as  contactsObject from "./contacts.json";
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  data: Contact[] = []; 
+load(){
+  const constJson = jsonfile.readFileSync("./contacts.json");
+  this.data = constJson;
+}
+getAll(){
+  return this.data;
+}
+addOne(contact: Contact){
+  return this.data.push(contact);
+}
+save(){
+  jsonfile.writeFileSync("./contacts.json", this.data);
+}
+getOneById(id){
+  const IdEncontrado = this.data.find((contacto)=>{
+    if(contacto.id == id){
+    return true;
+  }
+});
+return IdEncontrado;
+ }
+}
+ 
+export { ContactsCollection, Contact };
