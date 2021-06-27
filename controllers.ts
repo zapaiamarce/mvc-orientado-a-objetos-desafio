ae59a3d9084074399509f86394894a5509a2d7b2
pampero-iot <pampero.iot@gmail.com>
2021-06-26 21:49:51 -0300

    completo las tareas asignadas

--- controllers.ts
+++ controllers.ts
@@ -7,7 +7,20 @@ export class ContactsControllerOptions {
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    const contactsCollection = new ContactsCollection();
+    this.contacts = contactsCollection;
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+    if (options.action === "get") {
+      return (
+        this.contacts.getOneById(options.params.id) || this.contacts.getAll()
+      );
+    } else if (options.action === "save") {
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+    }
+  }
 }
 export { ContactsController };
