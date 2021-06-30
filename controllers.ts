aa5ab87db2de5f246392aa6b4dcda920b03e6db4
alayn saez <saezalayn@gmail.com>
2021-06-30 13:03:10 -0300

    Desafio resuelto

--- controllers.ts
+++ controllers.ts
@@ -1,13 +1,29 @@
 import { ContactsCollection } from "./models";
 
-export class ContactsControllerOptions {
+class ContactsControllerOptions {
   action: "get" | "save";
   params: any;
 }
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+
+  processOptions(options: ContactsControllerOptions) {
+    if (options.action === "get" && options.params.id) {
+      return this.contacts.getOneById(options.params.id);
+    } else if (options.action === "get") {
+      return this.contacts.getAll();
+    }
+
+    if (options.action === "save" && options.params) {
+      this.contacts.addOne(options.params);
+      return this.contacts.save();
+    }
+  }
 }
-export { ContactsController };
+export { ContactsController, ContactsControllerOptions };
