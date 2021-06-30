b831fb094a1bd4f12ecc92c8a3d4fe44225732fa
cynthia.perez <cynthiaperezdaubert@gmail.com>
2021-06-30 17:05:45 -0300

    desafio mvc orientado a objetos terminado

--- index.ts
+++ index.ts
@@ -1,13 +1,26 @@
+import { ContactsCollection } from "./models";
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+/* import * as minimist from "minimist"; */
 
 function parseaParams(argv): ContactsControllerOptions {
-  // parsear el argv usando https://www.npmjs.com/package/minimist
+  /*  const resultado = minimist(argv);
+  console.log(resultado); */
+  var argv = require("minimist")(process.argv.slice(2));
+
   return {
-    action: null,
-    params: null,
+    action: argv.action,
+    params: JSON.parse(argv.params),
   };
 }
 
-function main() {}
+function main() {
+  const controller = new ContactsController();
+  const params = parseaParams(process.argv);
+  //console.log("soy params", params);
+  const result = controller.processOptions(params);
+  console.log(result);
+  /* const coll = new ContactsCollection();
+  coll.load(); */
+}
 
 main();
