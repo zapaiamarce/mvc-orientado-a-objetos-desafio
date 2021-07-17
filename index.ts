0c80d62d175deb589b9bcac07488d96da79d3af3
Florencia <maluf.florencia@gmail.com>
2021-07-17 14:58:55 -0300

    MVC Desafío

--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import * as minimist from "minimist";
 
 function parseaParams(argv): ContactsControllerOptions {
-  // parsear el argv usando https://www.npmjs.com/package/minimist
+  const parseado = minimist(argv);
+
   return {
-    action: null,
-    params: null,
+    action: parseado.action,
+    params: JSON.parse(parseado.params),
   };
 }
 
-function main() {}
+function main() {
+  const controller = new ContactsController();
+  const params = parseaParams(process.argv.slice(2));
+  const resultado = controller.processOptions(params);
+  console.log(resultado);
+}
 
 main();
--- models.ts
+++ models.ts
@@ -1,7 +1,38 @@
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+
+  load() {
+    const contacts = jsonfile.readFileSync("./contacts.json");
+    this.data = contacts;
+  }
+
+  getAll() {
+    return this.data;
+  }
+
+  addOne(contact: Contact) {
+    this.data.push(contact);
+  }
+
+  save() {
+    jsonfile.writeFileSync("./contacts.json", this.data);
+  }
+
+  getOneById(id: number) {
+    var getOne;
+    this.data.find(function (contact) {
+      if (contact.id == id) {
+        getOne = contact;
+      }
+    });
+    return getOne;
+  }
+}
 export { ContactsCollection };
