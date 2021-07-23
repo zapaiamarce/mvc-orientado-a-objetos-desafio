6754238e6f990b84f121e7935feb292fd4a2bff0
ariel <arieldaich@gmail.com>
2021-07-23 15:58:59 -0300

    primer commit, tests passed

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,30 @@
 import test from "ava";
-import { ContactsController } from "./controllers";
+import { ContactsController, ContactsControllerOptions } from "./controllers";
+import { ContactsCollection } from "./models";
+import * as contacts from "./contacts.json"
 
-// test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+  const collection = new ContactsCollection;
+  collection.load();
+  const cController = new ContactsController;
+  t.deepEqual(cController.contacts.getAll(), collection.getAll());
+})
 
-// test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions", (t) => {
+  const newContact = { id: 5, name: "Ariel" };
+  const existingContact = contacts.find(c => c.id == 3);
+  
+  const cOptionsOne = new ContactsControllerOptions("get", "-");
+  const cOptionsTwo = new ContactsControllerOptions("get", 3);
+  const cOptionsThree = new ContactsControllerOptions("save", newContact);
+  
+  const cController = new ContactsController;
+
+  t.deepEqual(contacts, cController.processOptions(cOptionsOne));
+  t.deepEqual(existingContact, cController.processOptions(cOptionsTwo));
+
+  cController.processOptions(cOptionsThree);
+  const foundContact = cController.contacts.data.find(c => c == newContact); 
+
+  t.deepEqual(foundContact, newContact);
+})
--- controllers.ts
+++ controllers.ts
@@ -1,13 +1,29 @@
 import { ContactsCollection } from "./models";
 
 export class ContactsControllerOptions {
+  constructor(action: "get" | "save", params: any){
+    this.action = action;
+    this.params = params;
+  }
   action: "get" | "save";
   params: any;
 }
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    this.contacts = new ContactsCollection;
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+    if (options.action == "save") {
+      this.contacts.addOne(options.params);
+    }
+    if (options.action == "get" && this.contacts.data.find(c => c.id == options.params)) {
+      return this.contacts.data.find(c => c.id == options.params);
+    } else if (options.action == "get") {
+      return this.contacts.data;
+    }
+  }
 }
 export { ContactsController };
