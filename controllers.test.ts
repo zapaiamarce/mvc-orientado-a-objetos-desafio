5cbaaef283020ed75bc7c67bc0e1c6f77a2af33d
Zura-nw <dayb62083@gmail.com>
2021-07-30 00:37:56 -0300

    desafio completado(?

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,41 @@
 import test from "ava";
 import { ContactsController } from "./controllers";
+import { ContactsControllerOptions } from "./controllers"
+import * as jsonfile from "jsonfile"
+import { ContactsCollection } from "./models";
 
 // test("Testeo el constructor del controller", (t) => {});
 
+test("Testeo el constructor del controller", (t) => {
+    const controller = new ContactsController();
+    const ContactsCollection = controller.contacts;
+    ContactsCollection.load()
+
+    const mockContact = {
+        id: 37,
+        name: "Nahuel"
+    };
+
+    ContactsCollection.addOne(mockContact);
+    ContactsCollection.save();
+    const fileContent = jsonfile.readFileSync("./contacts.json")
+
+    t.deepEqual(fileContent, ContactsCollection.getAll());
+  });
+
 // test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions", (t) => {
+    const controllerOptions = new ContactsControllerOptions();
+    controllerOptions.action = "get";
+    controllerOptions.params = { id: 37 };
+    
+    const controller = new ContactsController();
+    const resultado = controller.processOptions(controllerOptions);
+    
+    const contacts = new ContactsCollection();
+    contacts.load();
+    const resultado2 = contacts.getOneById(37);
+
+    t.deepEqual(resultado, resultado2)
+    
+})
