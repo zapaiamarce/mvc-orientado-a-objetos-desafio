144ac928669567a5338d453d3c884be9acc3cb8b
Nicasio Ojeda Bianchi <NicaOjeda@outlook.com>
2021-08-02 18:05:34 -0300

    Se crearon los metodos load, getAll, addOne, save y getOneById en la clase ContactsCollection, se agrego a index.ts las utilidades para parsear argumentos desde terminal, controllers.ts en su clase ContactsController obtuvo el metodo processOptions que va ligado a index.ts para obtener, mostrar o agregar argumentos que cumplan con las condiciones de la clase ContactsControllerOptions, por ultimo, controller.test.ts obtuvo 2 test que fueron pasados correctamente.

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,23 @@
 import test from "ava";
 import { ContactsController } from "./controllers";
+import { ContactsControllerOptions } from "./controllers";
+import {ContactsCollection} from "./models";
 
-// test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+    var contController = new ContactsController();
+    var mock = new ContactsCollection();
+    mock.load();
+    t.deepEqual(contController.contacts,mock);
+});
 
-// test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions", (t) => {
+    var contController = new ContactsController();
+    var mock = new ContactsControllerOptions();
+    var ContCollection = new ContactsCollection();
+    ContCollection.load()
+    mock.action = "get";
+    mock.params = {
+        id : "23",
+    };
+    t.deepEqual(contController.processOptions(mock), ContCollection.getOneById(23));
+});
