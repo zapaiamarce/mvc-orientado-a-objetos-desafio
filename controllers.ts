bd0e5bc14699f7a53cd5c2d7ccf3406908d19e2b
Karen <karen_gonzalezm05@hotmail.com>
2021-08-17 11:45:16 -0300

    cambios que le hice para que funcione

--- controllers.ts
+++ controllers.ts
@@ -7,7 +7,20 @@ export class ContactsControllerOptions {
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+    if(options.action == "get" && options.params.id){
+return this.contacts.getOneById(options.params.id);}
+    else if(options.action == "get"){
+return this.contacts.getAll();}
+else if(options.action == "save"){
+  this.contacts.addOne(options.params);
+  return this.contacts.save();
+}
+
+  }
 }
 export { ContactsController };
--- models.ts
+++ models.ts
@@ -1,7 +1,46 @@
+import * as jsonfile from "jsonfile";
+import { fdatasync } from "node:fs";
+import { findSourceMap } from "node:module";
+
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+data: Contact[] = []; //se pone el array vacio xq sino no corre bien el test
+load(){
+  const json = jsonfile.readFileSync("./contacts.json");
+  this.data = json;}
+
+getAll(){
+return this.data;
+}
+addOne(contact:Contact){
+  return this.data.push(contact);
+}
+save(){
+
+  jsonfile.writeFileSync("./contacts.json", this.data);
+}
+getOneById(id){
+const encontrado = this.data.find((item)=>{
+  return item.id == id;
+})
+return encontrado;
+
+}
+  
+}
+
+
+
+
+
+
+
+
+
+
 export { ContactsCollection };
