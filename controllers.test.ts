78fc534c74ba512c83d9d7e591e183d9432a84e8
Fernando Areosa <fernando_areosa@hotmail.com.ar>
2021-09-26 00:57:55 -0300

    fix: Se resuelve el desafio con écito

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,30 @@
 import test from "ava";
-import { ContactsController } from "./controllers";
+import { ContactsController , ContactsControllerOptions } from "./controllers";
+import { ContactsCollection } from "./models";
 
-// test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+    const a= new ContactsController()
+    const b= new ContactsCollection()
+    b.load()
+    t.deepEqual(a.contacts.data,b.data)
 
-// test("Testeo el método processOptions", (t) => {});
+});
+
+test("Testeo el método processOptions", (t) => {
+    const a= new ContactsController();
+    const b = new ContactsControllerOptions();
+    const obj= {
+        "id": 5,
+        "name": "Fer"
+      };
+    b.action= "save";
+    b.params= obj 
+    a.processOptions(b);
+    t.deepEqual(a.contacts.data.find(i=>i.id==5),b.params);
+    b.action="get";
+    b.params= 5;
+    t.deepEqual(a.processOptions(b), obj );
+    b.params= undefined;
+    t.deepEqual(a.processOptions(b),a.contacts.data);
+       
+});
--- models.ts
+++ models.ts
@@ -1,7 +1,26 @@
+import * as jsonfile from "jsonfile"
+const fileContent = jsonfile.readFileSync("./contacts.json")
 class Contact {
   id: number;
   name: string;
-}
+};
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  data:Contact[]=[]
+  load(){     
+    fileContent.forEach(i=>{this.addOne(i)});
+    this.save()  
+  };
+  getAll():Contact[]{return this.data;}
+  addOne(contact:Contact) {
+    this.data.push(contact);
+    this.save();
+  };
+  save(){
+    jsonfile.writeFileSync("./contacts.json",this.data, { spaces: 2 })
+  };
+  getOneById(id:number){
+    return this.data.find(i=>i.id==id)    
+  };
+};
+export { ContactsCollection, Contact };
