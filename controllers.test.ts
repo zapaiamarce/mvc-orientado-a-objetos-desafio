08db44eb948725ee05a3bddec1829759356dc7c5
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-10-10 16:44:22 -0300

    desafio finalizado

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,37 @@
-import test from "ava";
-import { ContactsController } from "./controllers";
+import test from 'ava';
+import { ContactsCollection } from './models';
+import * as contactsObject from './contacts.json';
+import * as jsonfile from 'jsonfile';
 
-// test("Testeo el constructor del controller", (t) => {});
+test('Testeo el constructor del controller', (t) => {
+  // const contactsController = new ContactsController();
+  const model = new ContactsCollection();
+  model.load();
+  t.deepEqual(contactsObject, model.getAll());
+});
 
-// test("Testeo el método processOptions", (t) => {});
+test('Testeo el método processOptions', (t) => {
+  // const contactsController = new ContactsController();
+  const model = new ContactsCollection();
+  // Test getOneById
+  const mockContact = {
+    id: 32,
+    name: 'Misael Test Controller',
+  };
+  model.addOne(mockContact);
+  const one = model.getOneById(32);
+  t.deepEqual(one, mockContact);
+  // Test getAll
+  model.load();
+  const fileContent = jsonfile.readFileSync('./contacts.json');
+  t.deepEqual(fileContent, model.getAll());
+  // Test addOne
+  const mockContact2 = {
+    id: 33,
+    name: 'Pablo Test Controller',
+  };
+  model.addOne(mockContact2);
+  model.save();
+  const fileContent2 = jsonfile.readFileSync('./contacts.json');
+  t.deepEqual(fileContent2, model.getAll());
+});
--- models.ts
+++ models.ts
@@ -1,7 +1,27 @@
+import * as jsonfile from 'jsonfile';
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  contacts: Contact[] = [];
+  load() {
+    const json = jsonfile.readFileSync('./contacts.json');
+    this.contacts = json;
+  }
+  getAll(): Contact[] {
+    return this.contacts;
+  }
+  addOne(contact: Contact) {
+    this.contacts.push(contact);
+  }
+  save() {
+    jsonfile.writeFileSync('./contacts.json', this.contacts);
+  }
+  getOneById(id: number): Contact {
+    return this.contacts.find((contact) => contact.id === id);
+  }
+}
+
+export { ContactsCollection, Contact };
