7b75318a36cde54f8dfea0815e3fd366dd7e3a0a
Florencia Luz Duarte <florluzduarte@gmail.com>
2021-10-19 21:55:39 -0300

    resolucion controller.ts

--- controllers.ts
+++ controllers.ts
@@ -1,3 +1,4 @@
+import { isDeepStrictEqual } from "util";
 import { ContactsCollection } from "./models";
 
 export class ContactsControllerOptions {
@@ -6,8 +7,44 @@ export class ContactsControllerOptions {
 }
 
 class ContactsController {
+
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+
+  constructor() {
+    // instanciar ContactsCollection y guardarlo en una prop interna
+    this.contacts = new ContactsCollection();
+
+    // invocar al método load
+    //(para que se carge la data del archivo contacts.json)
+    this.contacts.load();
+  }
+
+  processOptions(options: ContactsControllerOptions) {
+    //dependiendo de las propiedades action y params debe interactuar con
+    //el modelo (ContactsController) e invocar distintos métodos
+
+    //en el caso de que el action sea "get" y el objeto params tenga un id,
+    //debe devolver un contacto en particular
+
+    if(options.action == "get" && options.params.id) {
+      this.contacts.getOneById(options.params.id)
+    };
+
+    //si id no existe significa que debe devolver todos los contactos
+
+    if(options.action == "get"){
+      this.contacts.getAll()
+    };
+
+    //en el caso de que action sea "save" params
+    //es lo que se debe usar como contacto nuevo
+
+    if(options.action == "save") {
+      this.contacts.addOne(options.params)
+    }
+
+  }
 }
+
+
 export { ContactsController };
