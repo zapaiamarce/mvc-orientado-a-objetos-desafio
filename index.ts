34808f67f76e1ae5a5879d8be5e61159b7eb2234
Your Name <you@example.com>
2021-10-27 18:16:28 -0300

    en teoria ya esta

--- index.ts
+++ index.ts
@@ -1,13 +1,21 @@
-import { ContactsController, ContactsControllerOptions } from "./controllers";
+import { ContactsController, ContactsControllerOptions } from './controllers';
 
 function parseaParams(argv): ContactsControllerOptions {
   // parsear el argv usando https://www.npmjs.com/package/minimist
+  const argvParsed = require('minimist')(argv.slice[2]);
   return {
-    action: null,
-    params: null,
+    action: argvParsed.action,
+    params: { id: argvParsed.id, name: argvParsed.name },
   };
 }
 
-function main() {}
+function main() {
+  const argv = process.argv;
+  const parameters = parseaParams(argv);
+
+  const controller = new ContactsController();
+  console.log(controller.processOptions(parameters));
+  console.log(parameters);
+}
 
 main();
