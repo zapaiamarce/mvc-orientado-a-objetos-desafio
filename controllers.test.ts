feb03dc5a3d1e5aa3e80e0d0c284f940d2defa37
Leandro <leandrolm87@gmail.com>
2021-11-03 16:56:08 -0300

    resolucion del desafio

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,15 @@
 import test from "ava";
-import { ContactsController } from "./controllers";
-
+import { ContactsController, ContactsControllerOptions } from "./controllers";
+import * as jsonfile from "jsonfile"
 // test("Testeo el constructor del controller", (t) => {});
 
-// test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions", (t) => {
+    const model = new ContactsController()
+    const fileContent = jsonfile.readFileSync(__dirname + "/contacts.json")
+    
+    const mockOptions = new ContactsControllerOptions()
+    mockOptions.action = "get"
+    mockOptions.params = false
+
+    t.deepEqual(model.processOptions(mockOptions), fileContent)
+});
