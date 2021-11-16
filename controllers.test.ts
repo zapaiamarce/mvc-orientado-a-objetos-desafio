fb0ca5458ffea1a331dc609944670251cc43fe68
Carlos <carreniocarlosgabriel@gmail.com>
2021-11-16 10:47:08 -0300

    testeando con el pr

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,17 @@
 import test from "ava";
-import { ContactsController } from "./controllers";
+import { ContactsController, ContactsControllerOptions } from "./controllers";
 
-// test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+  const controller = new ContactsController();
+  const load = controller.contacts.load();
+  t.deepEqual(load, controller.contacts.load());
+});
 
-// test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions", (t) => {
+  const contacto = new ContactsController();
+  const option = new ContactsControllerOptions();
+  option.action = "get";
+  option.params = 1;
+  const getContacto = contacto.processOptions(option);
+  t.deepEqual(getContacto, contacto.contacts.getOneById(1));
+});
--- models.ts
+++ models.ts
@@ -1,7 +1,27 @@
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+  load() {
+    const dataJson = jsonfile.readFileSync("./contacts.json");
+    this.data = dataJson;
+  }
+  getAll() {
+    return this.data;
+  }
+  addOne(contact: Contact) {
+    this.data.push(contact);
+  }
+  save() {
+    jsonfile.writeFile("./contacts.json", this.data);
+  }
+  getOneById(id: number) {
+    return this.data.find((contact) => contact.id == id);
+  }
+}
 export { ContactsCollection };
