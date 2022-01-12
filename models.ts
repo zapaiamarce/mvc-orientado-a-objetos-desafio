2fbc512056790b7d13317a23e202edb3a46cd036
Leandro Monteleone <monteleonelea@gmail.com>
2022-01-12 18:42:39 -0300

    Desafio terminado

--- models.ts
+++ models.ts
@@ -1,7 +1,34 @@
+import * as fs from "fs";
+import * as jsonfile from "jsonfile";
+
+const contactsJson = fs.readFileSync("./contacts.json").toString();
+var contacts = JSON.parse(contactsJson);
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  data: Contact[] = [];
+  load () {
+    this.data = contacts;
+  }
+  getAll(){
+    return this.data;
+  }
+  addOne(contact: Contact) {
+    this.data.push (contact);
+  }
+  save() {
+    jsonfile.writeFileSync("./contacts.json", this.data);
+  }
+  getOneId(id) {
+    const resultado = this.data.find(function(contact) {
+      return id == contact.id;
+    });
+    return resultado;
+  }
+};
+
+export {ContactsCollection};
