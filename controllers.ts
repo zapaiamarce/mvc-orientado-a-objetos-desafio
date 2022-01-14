f31c916d06a3aa37f3659da81fed1288d43cb28a
unknown <fa-ndo@hotmail.com>
2022-01-14 17:00:55 -0300

    Segundo Commit, andan todos los tests

--- controllers.ts
+++ controllers.ts
@@ -7,7 +7,19 @@ export class ContactsControllerOptions {
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+    const nuevoObj = new ContactsCollection();
+    if (options.action == "get" && options.params.id) {
+      return this.contacts.getOneById(options.params.id);
+    } else if (options.action == "get") {
+      return this.contacts.getAll();
+    } else if (options.action == "save" && options.params) {
+      return this.contacts.addOne(options.params), this.contacts.save();
+    }
+  }
 }
 export { ContactsController };
--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import * as minimist from "minimist";
 
 function parseaParams(argv): ContactsControllerOptions {
+  const argvThing = minimist(argv);
   // parsear el argv usando https://www.npmjs.com/package/minimist
   return {
-    action: null,
-    params: null,
+    action: argvThing.action,
+    params: JSON.parse(argvThing.params),
   };
 }
 
-function main() {}
+function main() {
+  const controller = new ContactsController();
+  const parseaParamas = parseaParams(process.argv.slice(2));
+  const respuesta = controller.processOptions(parseaParamas);
+  console.log(respuesta);
+}
 
 main();
