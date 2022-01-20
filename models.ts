5fb4c0bce576b8719d44497a7dca7de038a8635e
gabriel carreño <carrenocarlosgabriel@gmail.com>
2022-01-20 14:14:59 -0300

    2 test passed

--- models.ts
+++ models.ts
@@ -1,7 +1,26 @@
+import * as jsonfile from "jsonfile";
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = []; // no olvidarse de inicializar mis collection!!!
+  load() {
+    this.data = jsonfile.readFileSync("./contacts.json");
+  }
+  getAll() {
+    return this.data;
+  }
+  addOne(contact) {
+    this.data.push(contact);
+  }
+  save() {
+    jsonfile.writeFileSync("./contacts.json");
+  }
+  getOneById(id) {}
+}
+
+const con = new ContactsCollection();
+
 export { ContactsCollection };
