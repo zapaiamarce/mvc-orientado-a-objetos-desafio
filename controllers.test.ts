21a2d91801b6919d867943535a9d5c010e2a1214
Richard Miguel Irala <richardirala47@hotmail.com>
2022-01-20 21:48:01 -0300

    Desafio completado

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,26 @@
 import test from "ava";
-import { ContactsController } from "./controllers";
+import { ContactsController, ContactsControllerOptions } from "./controllers";
 
-// test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+  const model = new ContactsController();
+  t.deepEqual(model.contacts.arrayData, model.contacts.getAll());
+});
 
-// test("Testeo el método processOptions", (t) => {});
+//Test método processOptions
+test("Testeo el método processOptions", (t) => {
+  const model = new ContactsController();
+  const optionGet: ContactsControllerOptions = {
+    action: "get",
+    params: { id: 4 },
+  };
+  const optionSave: ContactsControllerOptions = {
+    action: "save",
+    params: { id: 12, name: "francisco" },
+  };
+  //testeo de la option Get
+  t.deepEqual(model.processOptions(optionGet), { id: 4, name: "Dana" });
+
+  //testeo del option save
+  model.processOptions(optionSave);
+  t.deepEqual(model.contacts.getOneById(12), { id: 12, name: "francisco" });
+});
