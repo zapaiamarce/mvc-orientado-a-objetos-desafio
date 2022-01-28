86e86e7467630ed1eafd1a5ca45175cf315ca3b5
unknown <bissonilaura@gmail.com>
2022-01-28 09:49:04 -0300

    models.ts

--- models.ts
+++ models.ts
@@ -1,7 +1,32 @@
+import * as jsonfile from "jsonfile"
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data : Contact [] = []
+  load(){
+    const contactosJson = jsonfile.readFileSync(__dirname + "/contacts.json")
+     this.data = contactosJson   
+
+  }
+
+  getAll(){
+     return this.data
+  }
+
+  addOne(contact) {
+    return  this.data.push(contact)   
+  }
+   save (){
+     jsonfile.writeFileSync("./contacts.json", this.data)
+   }
+   getOneById(id) {
+    return this.data.find((c)=>{ return c.id == id })
+
+  
+   }
+}
+
 export { ContactsCollection };
