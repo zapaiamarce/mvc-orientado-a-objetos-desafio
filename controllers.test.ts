7d15baa91b0722b01a6589bf874c701e57d94b88
Gabriel <gabriel.antonio.funes@outlook.com>
2022-02-11 07:15:33 -0300

    desafio resuelto

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,30 @@
 import test from "ava";
-import { ContactsController } from "./controllers";
+import { ContactsController, ContactsControllerOptions } from "./controllers";
+import { ContactsCollection } from "./models";
 
-// test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+  const controller = new ContactsController();
+  const contact = new ContactsCollection();
+  contact.load();
 
-// test("Testeo el método processOptions", (t) => {});
+  t.deepEqual(contact.getAll(), controller.contacts.getAll());
+});
+
+test("Testeo el método processOptions", (t) => {
+  const controller = new ContactsController();
+  const options = new ContactsControllerOptions();
+  const params = {
+    id: 48,
+    name: "name1",
+  };
+  options.action = "save";
+  options.params = params;
+
+  controller.processOptions(options);
+
+  options.action = "get";
+  options.params = 48;
+  const result = controller.processOptions(options);
+
+  t.deepEqual(result, params);
+});
