b64eaaad8a1a7d3bcdc6ad6a57775c16a5e341e8
Jimena <jimenafernandez.business@gmail.com>
2022-02-18 18:31:17 -0300

    Clases creadas y todos los tests pasan

--- controllers.ts
+++ controllers.ts
@@ -3,11 +3,32 @@ import { ContactsCollection } from "./models";
 export class ContactsControllerOptions {
   action: "get" | "save";
   params: any;
+  constructor(action: "get" | "save", params: any) {
+    this.action = action
+    this.params = params
+  }
 }
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    this.contacts = new ContactsCollection()
+    this.contacts.load()
+  }
+  processOptions(options: ContactsControllerOptions) {
+    var data = this.contacts
+    if (options.action == "get") {
+      var contactos = data.contacts
+      var idExiste = contactos.find(c => c.id == options.params)
+      if (idExiste) {
+        return idExiste
+      } else {
+        return data.contacts
+      }
+    } else {
+      data.addOne(options.params)
+      data.save()
+    }
+  }
 }
 export { ContactsController };
--- models.ts
+++ models.ts
@@ -1,7 +1,38 @@
+import * as fs from "fs"
+import { writeFileSync } from "fs";
+
 class Contact {
   id: number;
   name: string;
+  constructor(id: number, name: string) {
+    this.id = id
+    this.name = name
+  }
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  contacts: any[] = []
+  constructor() {}
+  load() {
+    var archivo = fs.readFileSync(__dirname + "/contacts.json")
+    var data = JSON.parse(archivo.toString())
+    data.forEach(c => {
+      this.addOne(c)
+    })
+    return data
+  }
+  getAll() {
+    return this.contacts
+  }
+  addOne(contact: Contact) {
+    this.contacts.push(contact)
+  }
+  save() {
+    var arrayContactos = JSON.stringify(this.contacts, null, 2)
+    writeFileSync(__dirname + "/contacts.json", arrayContactos)
+  }
+  getOneById(id: number) {
+    return this.contacts.find(c => c.id === id)
+  }
+}
 export { ContactsCollection };
