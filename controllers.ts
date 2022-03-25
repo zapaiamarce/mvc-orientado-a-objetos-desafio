40488cfc5f642b8925b31dbab0e1b454e0b0e586
frpulido <franciscopulidocal@gmail.com>
2022-03-25 01:12:28 -0300

    primer commit

--- controllers.ts
+++ controllers.ts
@@ -1,13 +1,32 @@
 import { ContactsCollection } from "./models";
 
-export class ContactsControllerOptions {
+class ContactsControllerOptions {
   action: "get" | "save";
   params: any;
 }
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+
+  processOptions(options: ContactsControllerOptions) {
+    const contactsIds = this.contacts.data.map((item) => item.id);
+    if (options.action == "save") {
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+      return this.contacts.getAll();
+    } else if (
+      options.action == "get" &&
+      contactsIds.includes(options.params)
+    ) {
+      return this.contacts.getOneById(options.params);
+    } else {
+      return this.contacts.getAll();
+    }
+  }
 }
-export { ContactsController };
+
+export { ContactsController, ContactsControllerOptions };
