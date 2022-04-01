539cc641fd9b53d74713d2d3e0223d5742670775
Veronica Isabel Pereira <47119996+Alfombrasvoladoras@users.noreply.github.com>
2022-04-01 14:55:08 -0300

    actualizados

--- controllers.ts
+++ controllers.ts
@@ -1,4 +1,4 @@
-import { ContactsCollection } from "./models";
+import { ContactsCollection, Contact } from "./models";
 
 export class ContactsControllerOptions {
   action: "get" | "save";
@@ -7,7 +7,21 @@ export class ContactsControllerOptions {
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+    var resultado;
+    if (options.action == "get" && options.params.id) {
+      resultado = this.contacts.getOneById(options.params.id);
+    } else if (options.action == "get") {
+      resultado = this.contacts.getAll();
+    } else if (options.action == "save" && options.params) {
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+    }
+    return resultado;
+  }
 }
 export { ContactsController };
