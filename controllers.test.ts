ce4ec1f3d298a98f945ef7d7978591963004f399
Jae <jaeladu1@gmail.com>
2022-04-10 17:40:20 -0300

    completado

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,38 @@
 import test from "ava";
 import { ContactsController } from "./controllers";
+import { readFileSync } from "fs";
 
-// test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+    const controller = new ContactsController()
 
-// test("Testeo el método processOptions", (t) => {});
+    const data = JSON.parse(readFileSync(__dirname + '/contacts.json').toString())
+
+    t.is(controller.contacts.data.length, data.length)
+    t.is(controller.contacts.data.find(e => e.id == 1).name, 'Ana')
+    t.is(controller.contacts.data.find(e => e.id == 4).name, 'Dana')
+});
+
+test("Testeo el método processOptions", (t) => {
+    const controller = new ContactsController()
+
+    const primerContacto = controller.processOptions(
+        {
+            action: 'get',
+            params: { id: 1 }
+        }
+    )
+    const todosLosContactos = controller.processOptions({
+        action: 'get', params: {}
+    })
+    const guardado = controller.processOptions(
+        {
+            action: 'save',
+            params: { id: 10, name: 'Jae' },
+
+        }
+    )
+    t.deepEqual(primerContacto, { id: 1, name: 'Ana' })
+    t.deepEqual(todosLosContactos[1], { id: 2, name: 'Paula' })
+    t.truthy(controller.contacts.data.length > 4)
+
+});
