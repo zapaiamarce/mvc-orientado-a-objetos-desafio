72109fa49d1b61bafa86b093f1a7d0129d9c2aba
Bigotito <mgabrieltbf@gmail.com>
2022-04-12 20:18:38 -0300

    act

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,28 @@
 import test from "ava";
 import { ContactsController } from "./controllers";
 
-// test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+    const controller = new ContactsController();
 
-// test("Testeo el método processOptions", (t) => {});
+    t.truthy(controller.contacts.__data.length);
+});
+
+test("Testeo el método processOptions", (t) => {
+    const controller = new ContactsController();
+
+    const tamanInicial = controller.contacts.__data.length;
+    const listaGetAll = controller.processOptions({action:"get",params:undefined});
+
+    t.is(tamanInicial, listaGetAll.length);
+    
+    var contactoParticular = controller.processOptions({action:"get",params:3});
+
+    t.deepEqual([{id:3,name:"Mer"}],contactoParticular);
+
+    controller.processOptions({action:"save",params:{id:123,name:"Prueba"}});
+    controller.processOptions({action:"get",params:undefined});
+
+    contactoParticular = controller.processOptions({action:"get",params:123});
+
+    t.deepEqual([{id:123,name:"Prueba"}],contactoParticular);
+});
--- models.ts
+++ models.ts
@@ -1,7 +1,48 @@
+import { readFileSync, writeFileSync } from 'fs'
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  constructor(){this.__data = [];};
+
+  load() {
+    try{
+      const lectura = readFileSync(__dirname + '/contacts.json');
+
+      this.__data = JSON.parse(lectura.toString());
+    }
+    catch (err){
+      throw err;
+    }
+  };
+
+  getAll(){
+    return this.__data;
+  };
+
+  addOne(contact){
+      this.__data.push(contact);
+  };
+
+  save(){
+    try{
+      writeFileSync(__dirname + '/contacts.json', JSON.stringify(this.__data,null,2));
+    }
+    catch(err){
+      throw err;
+    }
+  };
+
+  getOneById(id:number){
+    return this.__data.find((item:Contact) => {
+      return item["id"] == id;
+    });
+  }
+
+  __data: Contact[];
+}
+
 export { ContactsCollection };
