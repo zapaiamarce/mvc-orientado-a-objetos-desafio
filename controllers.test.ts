98890d3723e1844f5727098ab969d7088be19bdd
nicolascasmuz <nicolascasmuz@hotmail.com>
2022-04-13 19:53:52 -0300

    desafio-mvc-toma1

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,13 @@
 import test from "ava";
 import { ContactsController } from "./controllers";
+import * as jsonfile from "jsonfile";
 
 // test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+  const controller = new ContactsController();
+  const laData = jsonfile.readFileSync("./contacts.json");
+
+  t.deepEqual(controller.contacts.data, laData);
+});
 
 // test("Testeo el método processOptions", (t) => {});
--- controllers.ts
+++ controllers.ts
@@ -7,7 +7,20 @@ export class ContactsControllerOptions {
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+    var resultado = this.contacts.data.map((i) => i.id);
+    if (options.action == "get" && resultado.includes(options.params)) {
+      return this.contacts.getOneById(options.params);
+    } else if (options.action == "get") {
+      return this.contacts.getAll;
+    } else if (options.action == "save" && options.params) {
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+    }
+  }
 }
 export { ContactsController };
