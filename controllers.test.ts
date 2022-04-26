4cfed530be94245089327d546c81cbd8c48de1a1
Walter <daniwortiz003@gmail.com>
2022-04-26 20:17:15 -0300

    otro commit

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,21 @@
 import test from "ava";
-import { ContactsController } from "./controllers";
+import { ContactsController, ContactsControllerOptions } from "./controllers";
+import * as jsonfile from "jsonfile";
 
 // test("Testeo el constructor del controller", (t) => {});
+ test("Testeo el constructor del controller", (t) => {
+     const controller = new ContactsController()
+     const fileContent = jsonfile.readFileSync("./contacts.json");
+    t.deepEqual(controller.contacts.contacts, fileContent)
+ });
 
 // test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions", (t) => {
+    const controller = new ContactsController();
+    const fileContent = jsonfile.readFileSync("./contacts.json");
+
+    const mockOptions = new ContactsControllerOptions()
+    mockOptions.action = "get"
+    mockOptions.params = false
+    t.deepEqual(controller.processOptions(mockOptions), fileContent)
+});
