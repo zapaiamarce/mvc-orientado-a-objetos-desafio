8b7fc3dcc94f41810ac7f50e4a72ead0439e3c5d
imprenart <imprenart@gmail.com>
2022-04-27 20:15:43 -0300

    Primer commit

--- index.ts
+++ index.ts
@@ -1,13 +1,22 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import * as minimist from "minimist";
 
 function parseaParams(argv): ContactsControllerOptions {
-  // parsear el argv usando https://www.npmjs.com/package/minimist
+  const resultado = minimist(argv);
+
   return {
-    action: null,
-    params: null,
+    action: resultado.action,
+    params: JSON.parse(resultado.params),
+    // params: resultado.params,
   };
 }
 
-function main() {}
+function main() {
+  const controller = new ContactsController();
+  const resultado = controller.processOptions(
+    parseaParams(process.argv.slice(2))
+  );
+  console.log(resultado);
+}
 
 main();
--- models.ts
+++ models.ts
@@ -1,7 +1,30 @@
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+
+  load() {
+    this.data = jsonfile.readFileSync("./contacts.json");
+  }
+  getAll() {
+    return this.data;
+  }
+  addOne(contact: Contact) {
+    const contactoEncontrado = this.data.find((c) => c.id === contact.id);
+    if (!contactoEncontrado) {
+      this.data.push(contact);
+    }
+  }
+  save() {
+    jsonfile.writeFileSync("./contacts.json", this.data);
+  }
+  getOneById(id: number) {
+    return this.data.find((c) => c.id === id);
+  }
+}
 export { ContactsCollection };
