798d7e177186e9895f867cbc1675f8ebb090bdd8
imprenart <imprenart@gmail.com>
2022-04-28 12:27:06 -0300

    desafio ultimo aunque no pasen los test de github

--- models.ts
+++ models.ts
@@ -15,26 +15,28 @@ class ContactsCollection {
     return this.data;
   }
   addOne(contact: Contact) {
-    const contactoEncontrado = this.data.find((c) => c.id === contact.id);
-    if (!contactoEncontrado) {
-      this.data.push(contact);
-      console.log("Contacto agregado correctamente");
-      return;
-    }
-    console.log(
-      `Ya existe el id: ${contact.id}. No se puede agregar este contacto`
-    );
-    return;
+    // const contactoEncontrado = this.data.find((c) => c.id === contact.id);
+    // if (!contactoEncontrado) {
+    //   this.data.push(contact);
+    //   console.log("Contacto agregado correctamente");
+    //   return;
+    // }
+    // console.log(
+    //   `Ya existe el id: ${contact.id}. No se puede agregar este contacto`
+    // );
+    // return;
+    this.data.push(contact);
   }
   save() {
     jsonfile.writeFileSync("./contacts.json", this.data);
   }
   getOneById(id: number) {
     const resultado = this.data.find((c) => c.id === id);
-    if (resultado) {
-      return resultado;
-    }
-    console.log(`No se encontraron contactos con el id: ${id}`);
+    // if (resultado) {
+    //   return resultado;
+    // }
+    // console.log(`No se encontraron contactos con el id: ${id}`);
+    return resultado;
   }
 }
 export { ContactsCollection };
