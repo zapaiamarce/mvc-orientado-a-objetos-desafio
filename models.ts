f6157d7d6ead1f400f77c31745938d3206d7b9e7
Agustina <agustinamagallanez.gmail.com>
2022-05-13 13:58:45 -0300

    commit del desafio terminado

--- models.ts
+++ models.ts
@@ -1,7 +1,38 @@
+import * as jsonfile from "jsonfile";
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  arrayData:Contact[] = [];
+  
+  load(){
+    const json = jsonfile.readFileSync("./contacts.json");
+    this.arrayData = json;
+  }
+
+  getAll(){
+    return this.arrayData;
+  }
+
+  addOne(contact:Contact){
+    this.arrayData.push(contact);
+  }
+
+  save(){
+    jsonfile.writeFileSync("./contacts.json", this.arrayData);
+  }
+
+  getOneById(id){
+    const idEncontrado = this.arrayData.find((contact)=>{
+      if(contact.id == id){
+        return true;
+      }
+    });
+
+    return idEncontrado;
+  }
+
+}
 export { ContactsCollection };
