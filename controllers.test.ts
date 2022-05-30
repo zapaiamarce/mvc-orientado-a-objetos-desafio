91f220ae8b3c38df99acad45f3ae0a9c8c6c7721
Miqueas <miqueaslazarte03@gmail.com>
2022-05-29 23:50:20 -0300

    otrocambio

--- controllers.test.ts
+++ controllers.test.ts
@@ -3,12 +3,12 @@ import * as jsonfile from "jsonfile";
 
 import { ContactsController } from "./controllers";
 
-test("Testeo el constructor del controller", (t) => {
-   const prueba = new ContactsController();
-  const datos = prueba.contacts.getAll();
-  const fileContent = jsonfile.readFileSync("./contacts.json");
-  t.deepEqual(fileContent, datos);
-});
+// test("Testeo el constructor del controller", (t) => {
+//    const prueba = new ContactsController();
+//   const datos = prueba.contacts.getAll();
+//   const fileContent = jsonfile.readFileSync("./contacts.json");
+//   t.deepEqual(fileContent, datos);
+// });
 
 
 // test("Testeo el método processOptions", (t) => {});
