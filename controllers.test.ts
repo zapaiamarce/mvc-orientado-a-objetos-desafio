4d37883bf2c20bf8e33ff7481d48d01edae8da14
Miqueas <miqueaslazarte03@gmail.com>
2022-05-29 23:48:49 -0300

    desafioEnd

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,14 @@
 import test from "ava";
+import * as jsonfile from "jsonfile";
+
 import { ContactsController } from "./controllers";
 
-// test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+   const prueba = new ContactsController();
+  const datos = prueba.contacts.getAll();
+  const fileContent = jsonfile.readFileSync("./contacts.json");
+  t.deepEqual(fileContent, datos);
+});
+
 
 // test("Testeo el método processOptions", (t) => {});
