e6a1e42ecc55cf3097bd3c56e529c3ac98539680
PedroSanto <pedrosantolaya@hotmail.com>
2022-06-01 21:09:52 -0300

    desafio completado

--- index.ts
+++ index.ts
@@ -3,11 +3,17 @@ import { ContactsController, ContactsControllerOptions } from "./controllers";
 function parseaParams(argv): ContactsControllerOptions {
   // parsear el argv usando https://www.npmjs.com/package/minimist
   return {
-    action: null,
-    params: null,
+    action: argv.action,
+    params: argv.params,
   };
 }
 
-function main() {}
+function main() {
+  var argv = require("minimist")(process.argv.slice(2));
+  parseaParams(argv);
+  const controlador = new ContactsController();
+  const respuesta = controlador.processOptions(parseaParams(argv));
+  console.log(respuesta);
+}
 
 main();
--- models.ts
+++ models.ts
@@ -1,7 +1,38 @@
+import * as jsonfile from "jsonfile";
+let fs = require("fs");
+
 class Contact {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+  }
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+  load() {
+    const productos = JSON.parse(fs.readFileSync(__dirname + "/contacts.json"));
+    productos.forEach((element) => {
+      this.data.push(element);
+    });
+  }
+  getAll() {
+    return this.data;
+  }
+  addOne(contact) {
+    this.data.push(contact);
+  }
+  save() {
+    const file = "./contacts.json";
+    const obj = this.data;
+    jsonfile.writeFileSync(file, obj, { spaces: 2 });
+  }
+  getOneById(id) {
+    return this.data.find((i) => {
+      return i.id == id;
+    });
+  }
+}
 export { ContactsCollection };
