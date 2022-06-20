dd34eb7562910f70f59466e800dcf74bd507c67c
ChinoCribioli <ezequiel.cribioli@gmail.com>
2022-06-20 18:32:14 -0300

    commit del desafio m2-cap11

--- models.ts
+++ models.ts
@@ -1,7 +1,32 @@
+import * as jsonfile from "jsonfile"
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+  load() {
+    const json = jsonfile.readFileSync("./contacts.json");
+    return this.data = json;
+  }
+  getAll() {
+    return this.data;
+  }
+  addOne(contact) {
+    return this.data.push(contact);
+  }
+  save() {
+    jsonfile.writeFileSync("./contacts.json", this.data)
+  }
+  getOneById(id: number) {
+    return this.data.find((item) => {
+      if (item.id == id) {
+        return true;
+      }
+    } )
+  }
+}
+
 export { ContactsCollection };
