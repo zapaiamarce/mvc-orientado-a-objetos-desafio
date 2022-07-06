5d8b85d99cda784a26c5bf9cd8408c2ad866e657
Martin Sarmiento <sarmientomartin678@gmail.com>
2022-07-05 21:36:00 -0300

    cambios en el desafio

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,34 @@
-import test from "ava";
-import { ContactsController } from "./controllers";
+import test from 'ava';
+import { ContactsController } from './controllers';
+import * as jsonfile from 'jsonfile';
+import { json } from 'stream/consumers';
 
-// test("Testeo el constructor del controller", (t) => {});
+test('Testeo el constructor del controller', (t) => {
+  const controller = new ContactsController();
+  controller.contacts.load();
+  const data = controller.contacts.getAll();
+  const json = jsonfile.readFileSync('./contacts.json');
+  t.deepEqual(data, json);
+});
 
-// test("Testeo el método processOptions", (t) => {});
+test('Testeo el método processOptions', (t) => {
+  const controller = new ContactsController();
+  const optionsGet = controller.processOptions({
+    action: 'get',
+    params: {},
+  });
+  const optionsGetWithParams = controller.processOptions({
+    action: 'get',
+    params: { id: 1 },
+  });
+  const optionsSave = controller.processOptions({
+    action: 'save',
+    params: { id: 31, name: 'Marce' },
+  });
+
+  const data = jsonfile.readFileSync('./contacts.json');
+
+  t.deepEqual(data, optionsGet);
+  t.deepEqual(data[0], optionsGetWithParams);
+  t.deepEqual(controller.contacts.save[1], optionsSave);
+});
--- controllers.ts
+++ controllers.ts
@@ -1,13 +1,25 @@
-import { ContactsCollection } from "./models";
+import { ContactsCollection } from './models';
 
 export class ContactsControllerOptions {
-  action: "get" | "save";
+  action: 'get' | 'save';
   params: any;
 }
 
 class ContactsController {
   contacts: ContactsCollection;
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+    if (options.action == 'get' && options.params.id) {
+      return this.contacts.getOneById(options.params.id);
+    } else if (options.action == 'get') {
+      return this.contacts.getAll();
+    } else if (options.action == 'save' && options.params) {
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+    }
+  }
 }
 export { ContactsController };
