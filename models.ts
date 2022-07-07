35ac96bcf778d4c04b6ae162a8d1c090f9f37056
Guillenjulian <guille.julian1985@gmial.com>
2022-07-06 23:15:57 -0300

    en este commit subo los cambios para finalizar el desafio con un problema en el codigo para verificacion

--- models.ts
+++ models.ts
@@ -1,7 +1,28 @@
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  data: Contact[] = [];
+  load() {
+    const json = jsonfile.readFileSync("./contacts.json");
+    this.data = json;
+  }
+  getAll() {
+    return this.data;
+  }
+  addOne(contact: Contact) {
+    this.data.push(contact);
+  }
+  save() {
+    jsonfile.writeFileSync("./contacts.json", this.data);
+  }
+  getOneById(id: number) {
+    const idEncontrado = this.data.find((ids) => ids.id === id);
+    return idEncontrado;
+  }
+}
+export { ContactsCollection, Contact };
