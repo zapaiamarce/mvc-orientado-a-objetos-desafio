2108df31049314504b1273a8df2259120558b3a9
Felipe Ponce <feli.piponce@gmail.com>
2022-07-20 19:01:31 -0300

    Ahora si

--- controllers.test.ts
+++
@@ -1,9 +0,0 @@
-// import test from "ava";
-// import { ContactsController } from "./controllers";
-// import * as jsonfile from "jsonfile";
-
-// test("Testeo el constructor del controller", (t) => {
-//   const controllerPrueba = new ContactsController();
-//   const elJson = jsonfile.readFileSync("./contacts.json")
-//   t.is(controllerPrueba.contacts.datos, elJson);
-// });
