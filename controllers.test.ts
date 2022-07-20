287e3d2720bce7db3926ef206bbca7435604acc1
Felipe Ponce <feli.piponce@gmail.com>
2022-07-20 18:52:08 -0300

    desafio capitulo 11

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,6 +1,9 @@
 import test from "ava";
 import { ContactsController } from "./controllers";
+import * as jsonfile from "jsonfile";
 
-// test("Testeo el constructor del controller", (t) => {});
-
-// test("Testeo el método processOptions", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+  const controllerPrueba = new ContactsController();
+  const elJson = jsonfile.readFileSync("./contacts.json")
+  t.is(controllerPrueba.contacts.datos, elJson);
+});
--- models.ts
+++ models.ts
@@ -1,7 +1,30 @@
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number;
   name: string;
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  datos: Contact[] = [];
+  load(){
+    const contactsJson = jsonfile.readFileSync("./contacts.json");
+    this.datos = contactsJson;
+    };
+  getAll(){
+    return this.datos;
+  }
+  addOne(contact:Contact) {
+    this.datos.push(contact);
+  }
+  save(){
+    jsonfile.writeFileSync("./contacts.json", this.datos);
+  }
+  getOneById(id){
+    const aDevolver = this.datos.find((c)=> (c.id == id));
+    return aDevolver;
+  }
+}
+
+
 export { ContactsCollection };
