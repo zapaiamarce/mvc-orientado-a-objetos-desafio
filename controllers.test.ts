e63a35b6230e2fa835a7d4e9921d4c77517357d1
--replace <cursodeprogramacion@gmail.com>
2022-07-29 23:49:52 -0300

    completos controllers.test.ts

--- controllers.test.ts
+++ controllers.test.ts
@@ -1,10 +1,34 @@
+// VER SI EN DISCORD EXPLICAN COMO HACERLO.
 import test from "ava";
 import { ContactsController } from "./controllers";
+import * as jsonfile from "jsonfile";
 
-// test("Testeo el constructor del controller", (t) => {});
-test("Testeo el constructor del controller", (t) => {});
+test("Testeo el constructor del controller", (t) => {
+	const controllerContacts = new ContactsController();
+	const jsonFile = jsonfile.readFileSync("./contacts.json");
+	t.deepEqual(jsonFile, controllerContacts.contacts.listaDedata);
+});
 
-// test("Testeo el método processOptions", (t) => {});
-test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions", (t) => {
+	const controllerContacts = new ContactsController();
+	const jsonFile = jsonfile.readFileSync("./contacts.json");
 
-// TENGO QEU VER EL VIDEO DE TEST Y VER SE HACIAN.
+	const contactById = controllerContacts.processOptions({
+		action: "get",
+		params: 3,
+	});
+	t.deepEqual(contactById, { id: 3, name: "Mer" });
+
+	const contactByIdWithParamsUndefined =
+		controllerContacts.processOptions({
+			action: "get",
+			params: 80,
+		});
+	t.deepEqual(jsonFile, contactByIdWithParamsUndefined);
+
+	const addContact = controllerContacts.processOptions({
+		action: "save",
+		params: { id: 15, name: "Nicolas" },
+	});
+	t.deepEqual(addContact, controllerContacts.contacts.getOneById(15));
+});
