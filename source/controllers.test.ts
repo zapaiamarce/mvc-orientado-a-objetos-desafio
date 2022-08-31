02a5cdb4d601812629d58640dd75d006185a2c2c
Ezequiel <ezequielezequiel9@gmail.com>
2022-08-30 21:56:28 -0300

    test local save() no pasa

--- source/controllers.test.ts
+++ source/controllers.test.ts
@@ -2,8 +2,8 @@ import test from "ava";
 import { ContactsController } from "./controllers";
 
 test("Testeo el constructor del controller", (t) => {
-  // test de ejemplo
-  t.truthy(true);
+  const listaContactos = new ContactsController();
+  t.truthy(listaContactos.contacts);
 });
 
 // test("Testeo el método processOptions", (t) => {});
--- source/index.ts
+++ source/index.ts
@@ -2,12 +2,19 @@ import { ContactsController, ContactsControllerOptions } from "./controllers";
 
 function parseaParams(argv): ContactsControllerOptions {
   // parsear el argv usando https://www.npmjs.com/package/minimist
+  var argv = require("minimist")(process.argv.slice(2));
   return {
-    action: null,
-    params: null,
+    action: argv.action,
+    params: JSON.parse(argv.params),
   };
 }
 
-function main() {}
+function main() {
+  const contactoController = new ContactsController();
+  const argvTerminal = process.argv.slice(2);
+  const paramsTerminal = parseaParams(argvTerminal);
+  const resultado = contactoController.processOptions(paramsTerminal);
+  console.log(resultado);
+}
 
 main();
