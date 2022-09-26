e6e7b969314818c14ba9c783799355a40d86af72
Your Name <you@example.com>
2022-09-26 13:49:23 -0500

    mvc-desafio

--- source/controllers.ts
+++ source/controllers.ts
@@ -1,13 +1,28 @@
-// import { ContactsCollection } from "./models";
+import { ContactsCollection } from "./models";
 
 export type ContactsControllerOptions = {
-	action?: "get" | "save" | null;
-	params: any;
+  action?: "get" | "save" | null;
+  params: any;
 };
 
 class ContactsController {
-	// contacts: ContactsCollection;
-	constructor() {}
-	processOptions(options: ContactsControllerOptions) {}
+  contacts: ContactsCollection;
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+    var resultado;
+    console.log(options.params);
+    if (options.action == "get" && options.params.id) {
+      resultado = this.contacts.getOneById(options.params.id);
+    } else if (options.action == "get") {
+      resultado = this.contacts.getAll();
+    } else if (options.action == "save" && options.params) {
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+    }
+    return resultado;
+  }
 }
 export { ContactsController };
