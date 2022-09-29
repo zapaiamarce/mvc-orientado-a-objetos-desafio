dc312639f7de023acb42adc1da286eca9a1ea94a
unknown <alejoiglesias@outlook.com>
2022-09-29 13:49:21 -0300

    challenge solved

--- source/models.ts
+++ source/models.ts
@@ -1,7 +1,32 @@
+import * as contacts from "./contacts.json";
+import * as fs from "fs";
+
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  collection: Contact[] = [];
+
+  load() {
+    this.collection.push(...contacts);
+  }
+  getAll() {
+    return this.collection;
+  }
+  addOne(contact) {
+    this.collection.push(contact);
+  }
+  save() {
+    fs.writeFileSync(
+      __dirname + "/contacts.json",
+      JSON.stringify(this.collection)
+    );
+  }
+  getOneById(id) {
+    return this.collection.find((contact) => contact.id == id);
+  }
+}
+
+export { ContactsCollection, Contact };
