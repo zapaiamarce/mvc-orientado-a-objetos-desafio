e041ad979ee98c06db4689aef7258f86cbbd9046
Emanuel Caceres <emanuelcaceres010@gmail.com>
2022-10-03 14:37:19 -0300

    desafio terminado

--- source/controllers.ts
+++ source/controllers.ts
@@ -1,13 +1,29 @@
 // import { ContactsCollection } from "./models";
 
+import { ContactsCollection, Contact } from "./models";
+
 export type ContactsControllerOptions = {
 	action?: "get" | "save" | null;
 	params: any;
 };
 
 class ContactsController {
-	// contacts: ContactsCollection;
-	constructor() {}
-	processOptions(options: ContactsControllerOptions) {}
+	 contacts: ContactsCollection;
+	constructor() {
+		this.contacts = new ContactsCollection();
+		this.contacts.load();
+	}
+	processOptions(options: ContactsControllerOptions) {
+		let resultado;
+		if(options.action == "get" && options.params.id){
+			resultado = this.contacts.getOneById(options.params.id)
+		}else if(options.action == "get"){
+			resultado = this.contacts.getAll();
+		}else if(options.action == "save" && options.params){
+			this.contacts.addOne(options.params);
+			this.contacts.save();
+		}
+		return resultado;
+	}
 }
 export { ContactsController };
