d447d2bceddc5a90b114d49c4c692e31624f623c
Marcelo Zapaia <zapaiamarce@gmail.com>
2022-10-24 18:04:46 -0300

    Update controllers.ts

--- source/controllers.ts
+++ source/controllers.ts
@@ -1,13 +1,14 @@
-// import { ContactsCollection } from "./models";
+import { ContactsCollection } from "./models";
 
 export type ContactsControllerOptions = {
-	action?: "get" | "save" | null;
-	params: any;
+  action?: "get" | "save" | null;
+  params: any;
 };
 
 class ContactsController {
-	// contacts: ContactsCollection;
-	constructor() {}
-	processOptions(options: ContactsControllerOptions) {}
+  contacts: ContactsCollection = {};
+  constructor() {}
+  processOptions(options: ContactsControllerOptions) {}
 }
+
 export { ContactsController };
