35d853198cd2ddc9017d7316344392c4f9827519
frannjribatto <80211117+frannjribatto@users.noreply.github.com>
2022-10-26 21:00:31 -0300

    desafio 11 v.1.1

--- source/controllers.ts
+++ source/controllers.ts
@@ -8,7 +8,7 @@ export type ContactsControllerOptions = {
 
 
 class ContactsController {
-	contacts;
+	contacts: ContactsCollection = {} as ContactsCollection;
 	
 	constructor() {
 		this.contacts = new ContactsCollection();
