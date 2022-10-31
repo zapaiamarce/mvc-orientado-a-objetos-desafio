6701503468a5295028ab4a019975e1012d96f510
Tomas <arnotomas1@gmail.com>
2022-10-31 12:37:42 -0300

    Agregando nuevamente el yarn

--- source/controllers.test.ts
+++ source/controllers.test.ts
@@ -3,13 +3,12 @@ import * as jFile from "jsonfile";
 import { ContactsController } from "./controllers";
 import { ContactsCollection } from "./models";
 
-// test("Testeo el constructor del controller", (t) => {
-//   const contactsControll = new ContactsController();
-//   const contactsCollection = new ContactsCollection();
-//   const archivoJson = jFile.readFileSync("./contacts.json");
+test("Testeo el constructor del controller", (t) => {
+  const contactsControll = new ContactsController();
+  const contactsCollection = new ContactsCollection();
+  const archivoJson = jFile.readFileSync("./contacts.json");
 
-//   t.deepEqual(contactsCollection.data, archivoJson);
-//   t.truthy(true);
-// });
+  t.deepEqual(contactsCollection.data, archivoJson);
+});
 
 // test("Testeo el método processOptions", (t) => {});
