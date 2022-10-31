475a4ee5376a9059dc142dc2aedf6202211820d0
Tomas <arnotomas1@gmail.com>
2022-10-31 12:29:53 -0300

    Ejercicio completado en funcionamiento, menos los test locales

--- source/controllers.test.ts
+++ source/controllers.test.ts
@@ -1,9 +1,15 @@
 import test from "ava";
+import * as jFile from "jsonfile";
 import { ContactsController } from "./controllers";
+import { ContactsCollection } from "./models";
 
-test("Testeo el constructor del controller", (t) => {
-  // test de ejemplo
-  t.truthy(true);
-});
+// test("Testeo el constructor del controller", (t) => {
+//   const contactsControll = new ContactsController();
+//   const contactsCollection = new ContactsCollection();
+//   const archivoJson = jFile.readFileSync("./contacts.json");
+
+//   t.deepEqual(contactsCollection.data, archivoJson);
+//   t.truthy(true);
+// });
 
 // test("Testeo el método processOptions", (t) => {});
--- source/models.ts
+++ source/models.ts
@@ -1,7 +1,29 @@
+import * as jFile from "jsonfile";
+
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+
+  load() {
+    const archivoJson = jFile.readFileSync("./contacts.json");
+    this.data = archivoJson;
+  }
+  getAll() {
+    return this.data;
+  }
+  addOne(contact: Contact) {
+    this.data.push(contact);
+  }
+  save() {
+    jFile.writeFileSync("./contacts.json", this.data);
+  }
+  getOneById(id) {
+    return this.data.find((x) => x.id == id);
+  }
+}
+
 export { ContactsCollection };
