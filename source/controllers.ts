dd047ca79a22615843b8ed899b36e5efe650c2d8
Fede Vainstub <fedevainstube98@gmail.com>
2022-11-15 12:12:04 +0200

    termine desafio mvc-objetos

--- source/controllers.ts
+++ source/controllers.ts
@@ -2,13 +2,33 @@ import { ContactsCollection } from "./models";
 
 export type ContactsControllerOptions = {
   action?: "get" | "save" | null;
-  params: any;
+  params?: any;
 };
 
 class ContactsController {
-  contacts: ContactsCollection = {};
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  contacts;
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+
+    if (options.action == "get" && options.params.id != null) {
+      const contactoPorID = this.contacts.getOneById(options.params.id);
+      return contactoPorID;
+
+    } 
+    
+    if (options.action == "get" && options.params.id == null) {
+      return this.contacts.getAll();
+
+    }
+    
+    if (options.action == "save") {
+      return this.contacts.addOne(options.params);
+    }
+
+  }
 }
 
 export { ContactsController };
--- source/models.ts
+++ source/models.ts
@@ -1,7 +1,35 @@
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  arrayConLaData: Contact[] = [];
+
+  load() {
+    const archivoJson = jsonfile.readFileSync(__dirname + "/contacts.json");
+
+    this.arrayConLaData = archivoJson;
+  }
+
+  getAll() {
+    return this.arrayConLaData;
+  }
+
+  addOne(contact: Contact) {
+    return this.arrayConLaData.push(contact);
+  }
+  save() {
+    jsonfile.writeFileSync(__dirname + "/contacts.json", this.arrayConLaData);
+  }
+  getOneById(id: number) {
+    const numeroBuscadoConId = this.arrayConLaData.find((p) => {
+      return p.id == id;
+    });
+    return numeroBuscadoConId;
+  }
+}
+
+export { ContactsCollection, Contact };
