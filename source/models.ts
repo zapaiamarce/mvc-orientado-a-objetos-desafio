484ea33923377528fe8ed2e1fe119fce293c89d1
Luis Motoni <motoniluis@gmail.com>
2023-02-06 22:58:34 -0300

    Test pasados

--- source/models.ts
+++ source/models.ts
@@ -1,7 +1,28 @@
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+  
+  load() {
+    const fileData = jsonfile.readFileSync(__dirname + "/contacts.json");
+    this.data = fileData;
+  }
+  getAll(){
+    return this.data;
+  }
+  addOne(contact: Contact) {
+    this.data.push(contact);
+  }
+  save(){
+    jsonfile.writeFileSync(__dirname + "/contacts.json", this.data);
+  }
+  getOneById(id){
+    return this.data.find(contact => contact.id === id);
+  }
+}
 export { ContactsCollection };
