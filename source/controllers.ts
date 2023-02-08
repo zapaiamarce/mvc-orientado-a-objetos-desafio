31c96aeb7825c4f84340319f00e9b9e96750effe
Luis Motoni <motoniluis@gmail.com>
2023-02-07 21:44:28 -0300

    tercer commit

--- source/controllers.ts
+++ source/controllers.ts
@@ -6,9 +6,23 @@ export type ContactsControllerOptions = {
 };
 
 class ContactsController {
-  contacts: ContactsCollection = {};
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  contacts: ContactsCollection;
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+  processOptions(options: ContactsControllerOptions) {
+    let contact;
+    if (options.action == "get" && options.params.id) {
+      contact = this.contacts.getOneById(options.params.id);
+    } else if (options.action == "get") {
+      contact = this.contacts.getAll();
+    } else if (options.action == "save" && options.params) {
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+    }
+    return contact;
+  }
 }
 
 export { ContactsController };
--- source/models.ts
+++ source/models.ts
@@ -1,7 +1,30 @@
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+  constructor() {
+    this.data = [];
+  }
+  load() {
+    const fileData = jsonfile.readFileSync("./contacts.json");
+    this.data = fileData
+  }
+  getAll(){
+    return this.data;
+  }
+  addOne(contact: Contact) {
+    this.data.push(contact);
+  }
+  save(){
+    jsonfile.writeFileSync("./contacts.json", this.data);
+  }
+  getOneById(id){
+    return this.data.find(contact => contact.id === id);
+  }
+}
 export { ContactsCollection };
