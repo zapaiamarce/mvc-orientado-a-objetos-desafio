9d1234d6aabce610b9677a5f39f2244e2b74bc39
Cristian <cris.cerdan93@gmail.com>
2023-02-28 10:32:32 -0300

    primer commit

--- source/index.ts
+++ source/index.ts
@@ -1,13 +1,21 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import * as minimist from "minimist";
+import { ContactsCollection } from "./models";  
 
 function parseaParams(argv): ContactsControllerOptions {
-  // parsear el argv usando https://www.npmjs.com/package/minimist
-  return {
-    action: null,
-    params: null,
-  };
+  const resultado=minimist(argv);
+  return{
+    action:resultado.action,
+    params:JSON.parse(resultado.params),
+  }
 }
 
-function main() {}
-
+function main() {
+  const controller=new ContactsController();
+  const params=parseaParams(process.argv.slice(2));
+  
+  const x=controller.processOptions(params)
+  console.log(x)
+}
+  
 main();
--- source/models.ts
+++ source/models.ts
@@ -1,7 +1,29 @@
+import * as jsonfile from "jsonfile"
 class Contact {
-  id: number = 0;
-  name: string = "";
+  id: number=0 ;
+  name: string="" ;
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  data: Contact[]=[];
+  load(){
+    const json=jsonfile.readFileSync(__dirname+"/contacts.json");
+    
+    this.data=json;
+  };
+  getAll(){
+    return this.data;
+
+  }
+  addOne(contact: Contact) {
+    return this.data.push(contact);
+  }
+  save(){
+    jsonfile.writeFileSync(__dirname+"/contacts.json",this.data);
+  }
+  getOneById(id:number){
+    const resultado=this.data.find((c)=>c.id==id);
+    return resultado;
+  }
+}
+export { ContactsCollection}
