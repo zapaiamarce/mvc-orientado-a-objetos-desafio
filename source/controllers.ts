b8b8238f504d6bbc07a34c9b07f1949b3a614a0f
Mariano Garmendia <marianogarmendia77@gmail.com>
2023-07-12 15:35:09 -0300

    Los 4 test del model pasaron

--- source/controllers.ts
+++ source/controllers.ts
@@ -6,9 +6,17 @@ export type ContactsControllerOptions = {
 };
 
 class ContactsController {
-  contacts: ContactsCollection = {};
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  contacts: ContactsCollection ;
+  constructor() {
+    const contact = new ContactsCollection()
+    this.contacts = contact
+    contact.load()
+  }
+  processOptions(options: ContactsControllerOptions) {
+//     dependiendo de las propiedades action y params debe interactuar con el modelo (ContactsController) e invocar distintos métodos
+// en el caso de que el action sea get y el objeto params tenga un id, debe devolver un contacto en particular, si id no existe significa que debe devolver todos los contactos
+// en el caso de que action sea save, params es lo que se debe usar como contacto nuevo
+  }
 }
 
 export { ContactsController };
