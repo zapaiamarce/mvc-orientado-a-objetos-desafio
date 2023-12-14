b1a02fda76d85c1cda06af74360954bc7f1557ed
demianmln <demianmolina27@gmail.com>
2023-12-14 11:52:25 -0300

    desafio completado y test agregados

--- source/models.ts
+++ source/models.ts
@@ -1,7 +1,31 @@
+import * as jsonfile from "jsonfile";
+import * as path from "path";
+
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+  load() {
+    const filepath = path.resolve(__dirname, "./contacts.json");
+    const json = jsonfile.readFileSync(filepath);
+    this.data = json;
+  }
+  getAll() {
+    return this.data;
+  }
+  addOne(contact: Contact) {
+    this.data.push(contact);
+  }
+  save() {
+    const filepath = path.resolve(__dirname, "./contacts.json");
+    return jsonfile.writeFileSync(filepath, this.data);
+  }
+  getOneById(id) {
+    const encontrado = this.data.find((contact) => contact.id === id);
+    return encontrado;
+  }
+}
 export { ContactsCollection };
