d8d41fd3dd737dc8dd54d5e3b1cc0c033ae7e4b8
IvanAichino <ivanaichino@gmail.com>
2024-02-11 21:01:14 -0300

    Trabajo sobre consignas del ejercicio

--- source/index.ts
+++ source/index.ts
@@ -1,13 +1,25 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import * as minimist from 'minimist';
 
-function parseaParams(argv): ContactsControllerOptions {
+function parseaParams(argv: any): ContactsControllerOptions {
   // parsear el argv usando https://www.npmjs.com/package/minimist
-  return {
-    action: null,
-    params: null,
-  };
+  const params = {
+    action: argv._[0] ? argv._[0] : null,
+    params: argv._[1] ? argv._[1] : null 
+  }
+  return params;
 }
 
-function main() {}
+function main() {
+  const argv = minimist(process.argv.slice(2));
+  const contactsController = new ContactsController;
+  const options = parseaParams(argv);
+
+  const result = contactsController.processOptions(options);
+
+  if(result){
+    console.table(result);
+  };
+};
 
 main();
--- source/models.ts
+++ source/models.ts
@@ -1,7 +1,43 @@
+import * as jsonfile from 'jsonfile';
+
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  collection : Contact[] = [];
+  path : string = __dirname + '/contacts.json';
+
+  load(): void {
+    const data = jsonfile.readFileSync(this.path);
+    data.forEach((element: Contact) => {
+      this.collection.push(element)
+    });
+  };
+
+  getAll(): Contact[] {
+    return this.collection;
+  };
+
+  addOne(contact: Contact): void {
+    this.collection.push(contact);
+  };
+
+  save():void {
+    jsonfile.writeFileSync(this.path, this.collection);
+  };
+
+  getOneById(id: number): Contact|void {
+    const contact = this.collection.find((element: Contact) => {
+      return element.id === id;
+    });
+    if(!contact){
+      return console.log('No existe una entrada con el Id que ingresaste');
+    };
+    return contact;
+  };
+
+};
+
 export { ContactsCollection };
