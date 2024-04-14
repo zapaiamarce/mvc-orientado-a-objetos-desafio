8105c6c23c8005c2c668d1fcad860623be5872f0
Pedro Bellis <bellispedro@gmail.com>
2024-04-14 11:53:05 -0300

    ready

--- source/controllers.test.ts
+++ source/controllers.test.ts
@@ -1,9 +1,27 @@
 import test from "ava";
 import { ContactsController } from "./controllers";
+import { ContactsCollection } from "./models";
+import { Contact } from "./models";
+import * as fs from "fs";
 
 test("Testeo el constructor del controller", (t) => {
-  // test de ejemplo
-  t.truthy(true);
+  const controlador = new ContactsController;
+  
+  const contactos = fs.readFileSync(__dirname + '/contacts.json').toString();
+  const cont = JSON.parse(contactos);
+  t.deepEqual(controlador.contacts.arrayDeContactos, cont);
 });
 
-// test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptionsSave", (t) => {
+  const pedro = new Contact();
+  pedro.id = 7;
+  pedro.name = "pedro";
+  const controlador = new ContactsController;
+  controlador.processOptions({action:"save",params: pedro});
+  t.deepEqual(controlador.contacts.getOneById(7), pedro);
+});
+test("Testeo el método processOptionsGet", (t) => { 
+  const controlador = new ContactsController;
+  controlador.contacts.load;
+  t.deepEqual(controlador.processOptions({action:"get",params: 1}), {"id": 1,"name": "Ana"});
+});
