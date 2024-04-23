bb83e3fc39227467f961afa10d8c9fed75b446a4
Rossini Maximo <maximorossini2016@gmail.com>
2024-04-23 17:45:27 -0300

    params working

--- source/controllers.ts
+++ source/controllers.ts
@@ -2,13 +2,33 @@ import { ContactsCollection } from "./models";
 
 export type ContactsControllerOptions = {
   action?: "get" | "save" | null;
-  params: any;
+  params?: { id: number; name: string };
 };
 
 class ContactsController {
-  contacts: ContactsCollection = {};
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  contactsColl: ContactsCollection;
+  constructor() {
+    const contacts = new ContactsCollection();
+    contacts.load();
+    this.contactsColl = contacts;
+  }
+  processOptions(options: ContactsControllerOptions) {
+    if (options.action === "get" && options.params?.id) {
+      const res = this.contactsColl.getOneById(options.params.id);
+      return res;
+    }
+
+    if (
+      options.action === "save" &&
+      options.params?.id &&
+      options.params?.name
+    ) {
+      this.contactsColl.addOne(options.params);
+      this.contactsColl.save();
+      return this.contactsColl.getAll();
+    }
+    return { error: "Error" };
+  }
 }
 
 export { ContactsController };
--- source/models.ts
+++ source/models.ts
@@ -1,7 +1,45 @@
+import * as contacts from "./contacts.json";
+import * as jsonfile from "jsonfile";
+
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  contactsColl: { id: number; name: string }[] = [];
+  load() {
+    this.contactsColl = contacts;
+    if (this.contactsColl.length > 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  getAll() {
+    return this.contactsColl;
+  }
+  addOne(contact: Contact) {
+    this.contactsColl.push(contact);
+  }
+  save() {
+    const file = "source/contacts.json";
+    jsonfile.writeFileSync(file, this.contactsColl);
+    return true;
+  }
+  getOneById(id: number) {
+    const contacts = this.getAll();
+    const res = contacts.find((contact: Contact) => {
+      if (contact.id == id) {
+        return contact;
+      }
+    });
+    if (res !== undefined) {
+      return res;
+    } else {
+      return contacts;
+    }
+  }
+}
+
 export { ContactsCollection };
