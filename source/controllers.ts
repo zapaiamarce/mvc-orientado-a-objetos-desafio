179b29b80ad52c5a5375c1a1c983c11130332781
matiasbermudez <mabgrc@hotmail.com>
2024-06-03 21:03:44 -0300

    MVC 2dointento

--- source/controllers.ts
+++ source/controllers.ts
@@ -6,9 +6,26 @@ export type ContactsControllerOptions = {
 };
 
 class ContactsController {
-  contacts: ContactsCollection = {};
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  contacts: ContactsCollection ;
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load()
+  }
+  processOptions(options: ContactsControllerOptions) {
+    
+    if(options.action == "get" && options.params.id){
+     
+      return this.contacts.getOneById(options.params.id)
+    }else if ( options.action == "get" ){
+      return this.contacts.getAll()
+    }else if (options.action == "save"  && options.params.id){
+      this.contacts.addOne(options.params)
+      this.contacts.save()
+    }
+    return
+  }
 }
+
+
 
 export { ContactsController };
--- source/index.ts
+++ source/index.ts
@@ -1,13 +1,21 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import { Contact } from "./models";
+import * as minimist from "minimist"
 
-function parseaParams(argv): ContactsControllerOptions {
-  // parsear el argv usando https://www.npmjs.com/package/minimist
+const resultado = minimist(process.argv.slice(2))
+
+function parseaParams(dato): ContactsControllerOptions {
+  
   return {
-    action: null,
-    params: null,
+    action: dato[1][0],
+    params: JSON.parse(dato[2][0])
   };
 }
 
-function main() {}
+function main() {
+  const ejemplo = parseaParams(Object.entries(resultado))
+  const opciones = new ContactsController()
+  opciones.processOptions(ejemplo)
+}
 
 main();
