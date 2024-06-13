f62d8c0d6b4def618cd7ad56f04fab212c1725f4
soydat <classicwtink@gmail.com>
2024-06-13 18:12:17 -0300

    Code Exercise Fixed

--- source/controllers.test.ts
+++ source/controllers.test.ts
@@ -1,9 +1,48 @@
 import test from "ava";
-import { ContactsController } from "./controllers";
+import { ContactsController} from "./controllers";
+import { ContactsCollection } from "./models";
 
 test("Testeo el constructor del controller", (t) => {
-  // test de ejemplo
-  t.truthy(true);
+  const contactosLista = new ContactsCollection();
+  const testController = new ContactsController(contactosLista);
+  t.truthy(testController.contacts.getAll().length > 0);
 });
 
-// test("Testeo el método processOptions", (t) => {});
+test("Testeo el método processOptions con action 'get' y params no null", (t) => {
+  const contactosLista = new ContactsCollection();
+  const testController = new ContactsController(contactosLista);
+
+  const newContact = {
+    "id": 400,
+    "name": "Nuevo Contacto"
+  }
+  const options = { action: "get" as "get", params: newContact};
+  const result = testController.processOptions(options);
+
+  t.deepEqual(result, contactosLista.getOneById(400));
+});
+
+test("Testeo el método processOptions con action 'save' y params siendo un nuevo contacto", (t) => {
+  const contactosLista = new ContactsCollection();
+  const testController = new ContactsController(contactosLista);
+
+  const newContact = {id:1234, name:"Nuevo Contacto"};
+  const options = { action: "save" as "save", params:newContact};
+  testController.processOptions(options);
+
+  t.deepEqual(testController.contacts.getOneById(1234), newContact);
+});
+
+test("Testeo el metodo proceso con action Null ", (t) => {
+  const contactoLista = new ContactsCollection();
+  const testController = new ContactsController(contactoLista);
+
+  const newContact = {
+    "id": 400,
+    "name": "Nuevo Contacto"
+  }
+  const options = { action: null, params: newContact};
+  const result = testController.processOptions(options);
+
+    t.deepEqual(result, undefined);
+})
--- source/controllers.ts
+++ source/controllers.ts
@@ -1,14 +1,30 @@
-import { ContactsCollection } from "./models";
+import { Contact, ContactsCollection } from "./models";
 
 export type ContactsControllerOptions = {
   action?: "get" | "save" | null;
-  params: any;
+  params: Contact;
 };
 
 class ContactsController {
-  contacts: ContactsCollection = {};
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
-}
+  contacts: ContactsCollection;
 
+  constructor(contactsCollection:ContactsCollection) {
+    this.contacts = contactsCollection
+    this.contacts.load()
+  }
+
+
+  processOptions(options: ContactsControllerOptions) {
+    var resultado;
+    if (options.action === "get" && options.params.id) {
+     resultado = this.contacts.getOneById(options.params.id);
+    } else if (options.action === "get") {
+      resultado = this.contacts.getAll();
+    } else if (options.action === "save" && options.params) {
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+    }
+    return resultado;
+  }
+}
 export { ContactsController };
