a7d0a9c084ea797b9bbe81c00dba026b4c6568bf
lucacarena97 <lucacarena98@gmail.com>
2024-06-26 16:03:26 -0300

    finalizado

--- source/controllers.ts
+++ source/controllers.ts
@@ -6,9 +6,28 @@ export type ContactsControllerOptions = {
 };
 
 class ContactsController {
-  contacts: ContactsCollection = {};
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  contacts: ContactsCollection;
+
+  constructor() {
+    this.contacts = new ContactsCollection();
+    this.contacts.load();
+  }
+
+  processOptions(options: ContactsControllerOptions) {
+    if (options.action === "get") {
+      if (options.params.id) {
+        return this.contacts.getOneById(options.params.id);
+      } else {
+        return this.contacts.getAll();
+      }
+    } else if (options.action === "save") {
+      this.contacts.addOne(options.params);
+      this.contacts.save();
+      return { message: "Contacto agregado y guardado correctamente" };
+    } else {
+      return { message: "Acción no válida" };
+    }
+  }
 }
 
 export { ContactsController };
--- source/index.ts
+++ source/index.ts
@@ -1,13 +1,21 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import minimist from "minimist";
 
-function parseaParams(argv): ContactsControllerOptions {
-  // parsear el argv usando https://www.npmjs.com/package/minimist
+function parseaParams(argv: any): ContactsControllerOptions {
+  const resultado = minimist(argv);
+  console.log(resultado);
+
   return {
-    action: null,
-    params: null,
+    action: resultado.action,
+    params: JSON.parse(resultado.params),
   };
 }
 
-function main() {}
+function main() {
+  const controller = new ContactsController();
+  const params = parseaParams(process.argv.slice(2));
+  const resultado = controller.processOptions(params);
+  console.log(resultado);
+}
 
 main();
