3559dbc4271106d9607fb51bc5a4e856c013e617
Marcelo Zapaia <zapaiamarce@gmail.com>
2024-07-08 10:10:46 -0300

    update dependencies

--- source/models.ts
+++ source/models.ts
@@ -1,3 +1,8 @@
+// este import existe solo para que tsc lo tome y lo copie a /build
+import "./contacts.json";
+// si no estuviera este import typescript no se da cuenta que lo necesitamos
+// ya que para escribir o leer al archivo usamos la libreria "jsonfile"
+
 class Contact {
   id: number = 0;
   name: string = "";
