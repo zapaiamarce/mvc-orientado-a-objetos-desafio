861f7c8003f90650cba21294fca3f963c5b13120
julian moyano <julianmoyano26@gmail.com>
2024-07-09 00:44:52 -0300

    por fin

--- source/models.ts
+++ source/models.ts
@@ -1,12 +1,32 @@
-// este import existe solo para que tsc lo tome y lo copie a /build
-import "./contacts.json";
-// si no estuviera este import typescript no se da cuenta que lo necesitamos
-// ya que para escribir o leer al archivo usamos la libreria "jsonfile"
+import "./contacts.json"
+import * as jsonfile from "jsonfile";
 
 class Contact {
   id: number = 0;
   name: string = "";
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+class ContactsCollection {
+  data: Contact[] = [];
+  load() {
+    const json = jsonfile.readFileSync(__dirname + "/contacts.json");
+    this.data = json;
+  }
+  getAll() {
+    return this.data;
+  }
+  addOne(contact: Contact) {
+    this.data.push(contact);
+  }
+  save() {
+    jsonfile.writeFileSync(__dirname + "/contacts.json", this.data);
+  }
+  getOneById(id: number) {
+    const finded = this.data.find((c) => {
+      return c.id == id;
+    });
+    return finded;
+  }
+}
+
+export { ContactsCollection, Contact };
