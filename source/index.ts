ffb10f2d6e8609070c5ce652d7fa40416fe3ebaf
Facundo Rodriguez <fr8080342@gmail.com>
2024-07-13 12:16:48 -0300

    13-7. Facundo Rodriguez

--- source/index.ts
+++ source/index.ts
@@ -1,13 +1,22 @@
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import { ContactsCollection, Contact } from "./models";
+import * as minimist from 'minimist';
 
-function parseaParams(argv): ContactsControllerOptions {
-  // parsear el argv usando https://www.npmjs.com/package/minimist
-  return {
-    action: null,
-    params: null,
-  };
+function parseaParams(argv: any): ContactsControllerOptions {
+     let resultado: any = minimist(argv);
+     return {
+         action: resultado.action,
+         params: resultado.params }
+}
+  
+function main() {
+     let output: Object = process.argv.slice(2);
+     let outputProcesado: ContactsControllerOptions = parseaParams(output);
+     let cel: ContactsController = new ContactsController();
+     let dato: any = cel.processOptions(outputProcesado);
 }
 
-function main() {}
+ 
 
-main();
+
+
--- source/models.ts
+++ source/models.ts
@@ -1,12 +1,40 @@
 // este import existe solo para que tsc lo tome y lo copie a /build
 import "./contacts.json";
+import * as jsonfile from 'jsonfile';
 // si no estuviera este import typescript no se da cuenta que lo necesitamos
 // ya que para escribir o leer al archivo usamos la libreria "jsonfile"
 
 class Contact {
-  id: number = 0;
-  name: string = "";
+   id: number = 0;
+   name: string = "Juan" }
+
+class ContactsCollection {
+   data: Contact[] = [];
+   constructor () {
+     this.data }
+
+   load() {
+     let resultado: Contact[] = jsonfile.readFileSync(__dirname + "/contacts.json");
+     this.data = resultado } 
+
+   getAll(): Contact[] {
+     return this.data }
+
+   addOne(contacto: any) {
+     this.data.push(contacto) }
+
+   save() {
+     jsonfile.writeFileSync(__dirname + "/contacts.json", this.data) }
+
+   getOneById(id: number) {
+     let aux: any;
+     let dato: boolean = false;
+     for(aux of this.data) {
+       if ( aux.id == id ) {
+         dato = true;
+         return aux } 
+     };
+     if (dato == false) { return null }  }
 }
 
-class ContactsCollection {}
-export { ContactsCollection };
+export {ContactsCollection, Contact};
