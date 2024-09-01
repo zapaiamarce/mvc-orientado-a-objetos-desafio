90c7cc9d0016eca7c659c25d29ca8fa9752881f6
Santia <santiagogmuller@gmail.com>
2024-08-31 22:07:06 -0300

    caombis

--- source/models.ts
+++ source/models.ts
@@ -1,6 +1,7 @@
 // este import existe solo para que tsc lo tome y lo copie a /build
 import "./contacts.json";
 import * as fs from "fs"; 
+import * as jsonfile from "jsonfile"
 // si no estuviera este import typescript no se da cuenta que lo necesitamos
 // ya que para escribir o leer al archivo usamos la libreria "jsonfile"
 
@@ -10,15 +11,15 @@ class Contact {
 }
 
 class ContactsCollection {
-  data : Contact [] 
-  constructor(data : Contact []) {
-    this.data = data
+  data : Contact [] = []
+  //constructor(data : Contact []) {
+  //  this.data = data
 
-  }
+  //}
   load() {
-    const datosBuffer = fs.readFileSync("./contacts.json")
-    const datosString = datosBuffer.toString()
-    this.data = JSON.parse(datosString);
+    //const datosBuffer = fs.readFileSync("./contacts.json")
+    //const datosString = datosBuffer.toString()
+    this.data =  jsonfile.readFileSync("./contacts.json")//JSON.parse(datosString);
   }
   getAll() {
     return this.data
