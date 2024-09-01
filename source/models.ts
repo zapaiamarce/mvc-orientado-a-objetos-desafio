a5f19d3f22589866e2c5a0abb744eb331b6944db
Santia <santiagogmuller@gmail.com>
2024-08-31 22:39:47 -0300

    otra boludoez

--- source/models.ts
+++ source/models.ts
@@ -29,8 +29,9 @@ class ContactsCollection {
     return this.data
   } 
   save() {
-    const dataParaElJson = JSON.stringify(this.data)
-    fs.writeFileSync ("./contacts.json", dataParaElJson)
+    //const dataParaElJson = JSON.stringify(this.data)
+    //fs.writeFileSync ("./contacts.json", dataParaElJson)
+    jsonfile.writeFileSync("./contacts.json ", this.data)
   }
   getOneById(id: number) {
     return this.data.find((obj) => obj.id === id)
