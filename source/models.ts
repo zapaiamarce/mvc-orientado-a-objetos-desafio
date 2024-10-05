386c35d94dc94dfca7ee4977e41e8856dab2e79c
david <davidabsenta@gmail.com>
2024-10-05 13:52:42 -0300

    resolucion desafio MVC

--- source/models.ts
+++ source/models.ts
@@ -1,5 +1,6 @@
 // este import existe solo para que tsc lo tome y lo copie a /build
 import "./contacts.json";
+import * as jsonfile from "jsonfile";
 // si no estuviera este import typescript no se da cuenta que lo necesitamos
 // ya que para escribir o leer al archivo usamos la libreria "jsonfile"
 
@@ -8,5 +9,26 @@ class Contact {
   name: string = "";
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+  data: Contact[] = [];
+
+  load() {
+    const leerData = jsonfile.readFileSync("contacts.json");
+    this.data = leerData;
+  }
+  getAll() {
+    return this.data;
+  }
+
+  addOne(contact: Contact) {
+    this.data.push(contact);
+  }
+  save() {
+    jsonfile.writeFileSync("contacts.json", this.data);
+  }
+  getOneById(id: number): Contact | undefined {
+    return this.data.find((p) => p.id === id);
+  }
+}
+
 export { ContactsCollection };
