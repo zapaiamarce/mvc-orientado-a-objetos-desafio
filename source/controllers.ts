bd86edf39e42f1ea7ff85f70e655df8642a5507d
Ale <ale.villa.0296@gmail.com>
2025-01-21 09:42:24 -0300

    Avance en la resolucion

--- source/controllers.ts
+++ source/controllers.ts
@@ -1,14 +1,38 @@
 import { ContactsCollection } from "./models";
+import data from "./contacts.json";
 
 export type ContactsControllerOptions = {
-  action?: "get" | "save" | null;
-  params: any;
+    action?: "get" | "save" | null;
+    params: any;
 };
 
 class ContactsController {
-  contacts: ContactsCollection = {};
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+    contacts: ContactsCollection;
+
+    constructor() {
+        this.contacts = new ContactsCollection();
+        this.contacts.load();
+    }
+
+    // En este caso cada camino debe retornar algo de forma explicita y se debe usar el else de forma obligatoria
+    // Caso contrario nos dara el error "Not all code paths return a value."
+    processOptions(options: ContactsControllerOptions) {
+        if (options.action === "save") {
+            this.contacts.addOne(options.params);
+            return "Contacto guardado";
+        } else if (options.action === "get") {
+            if (typeof options.params === "number") {
+                const retVal = this.contacts.getOneById(options.params);
+                return retVal;
+            }
+            return this.contacts.getAll();
+        } else {
+            return "No hay una accion valida";
+        }
+    }
 }
 
+const tes = new ContactsController();
+
+console.log(tes);
 export { ContactsController };
--- source/models.ts
+++ source/models.ts
@@ -2,11 +2,66 @@
 import "./contacts.json";
 // si no estuviera este import typescript no se da cuenta que lo necesitamos
 // ya que para escribir o leer al archivo usamos la libreria "jsonfile"
+import * as jsonfile from "jsonfile";
+import { find } from "lodash";
+import fs from "fs";
 
 class Contact {
-  id: number = 0;
-  name: string = "";
+    id: number = 0;
+    name: string = "";
 }
 
-class ContactsCollection {}
+class ContactsCollection {
+    contacts: Contact[] = [];
+
+    load() {
+        const data = jsonfile.readFileSync(__dirname + "/contacts.json");
+        data.map((contact: Contact) => {
+            this.addOne(contact);
+        });
+    }
+
+    getAll(): Contact[] {
+        return [...this.contacts];
+    }
+
+    addOne(contact: Contact) {
+        this.contacts.push(contact);
+    }
+
+    save() {
+        // Convertimos los contactos a formato JSON
+        const data = JSON.stringify(this.contacts, null, 2);
+        console.log(data);
+
+        // Escribimos el archivo contacts.json
+        // fs.writeFileSync("contacts.json", data, "utf8");
+        jsonfile.writeFileSync(__dirname + "/contacts.json", this.contacts, { spaces: 2 });
+    }
+
+    getOneById(id: number) {
+        const ret = find(this.contacts, (contact) => contact.id === id);
+        return ret;
+    }
+}
+
+// const res = new ContactsCollection();
+// res.load();
+// const data = jsonfile.readFileSync(__dirname + "/contacts.json");
+// console.log(res.getAll());
+// console.log(data);
+
+// const mockContact = {
+//     id: 30,
+//     name: "Marce",
+// };
+
+// const data1 = JSON.stringify(res, null, 2);
+// jsonfile.writeFile("C:/Users/Ale/Documents/Code/apx/Typescript/mvc-orientado-a-objetos-desafio/contacts.json", data, { spaces: 2 });
+
+// console.log(data1);
+
+// res.addOne(mockContact);
+// console.log(res.getAll());
+
 export { ContactsCollection };
