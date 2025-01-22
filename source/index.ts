7aa05b45207083dd533714f8014657c1505accf2
david21rios <121947579+david21rios@users.noreply.github.com>
2025-01-22 17:28:40 +0100

    index const params

--- source/index.ts
+++ source/index.ts
@@ -3,9 +3,10 @@ import * as minimist from "minimist";
 
 function parseaParams(argv): ContactsControllerOptions {
   const resultado = minimist(argv);
+  const params = resultado.params ? JSON.parse(resultado.params) : {};
   return {
     action: resultado.action,
-    params: JSON.parse(resultado.params),
+    params,
   };
 }
 
