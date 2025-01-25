b19c02cbb74b5b5dee2b8e08a1df449d414d6379
Constanza Serra <constanza_serra@hotmail.com>
2025-01-25 01:43:59 -0300

    Desafío terminado, todos los test pasan

--- source/controllers.ts
+++ source/controllers.ts
@@ -1,14 +1,34 @@
 import { ContactsCollection } from "./models";
 
 export type ContactsControllerOptions = {
-  action?: "get" | "save" | null;
-  params: any;
+  action?: "get" | "save" | null; // 'action' es una propiedad opcional que puede ser "get", "save" o null.
+  params: any; // 'params' es una propiedad obligatoria que puede ser de cualquier tipo.
 };
 
 class ContactsController {
-  contacts: ContactsCollection = {};
-  constructor() {}
-  processOptions(options: ContactsControllerOptions) {}
+  contacts: ContactsCollection;
+  
+  constructor() {
+    this.contacts =  new ContactsCollection();
+    this.contacts.load();
+  }
+
+  processOptions(options: ContactsControllerOptions) {
+    var resultado;
+    if (options.action === null) {
+      // Lógica para manejar el caso donde no se realiza ninguna acción, por ahora no hago nada
+    } else if (options.action === "get" && options.params.id) {           
+        resultado = this.contacts.getOneById(options.params.id)
+      } else if (options.action === "get" && !options.params.id){
+        resultado =  this.contacts.getAll();    
+    } else if (options.action === "save" && options.params) {      
+        resultado = this.contacts.addOne(options.params)
+        this.contacts.save();
+    }
+    return resultado;
+  }
+
+  
 }
 
 export { ContactsController };
--- source/index.ts
+++ source/index.ts
@@ -1,13 +1,24 @@
+import { argv } from "process";
 import { ContactsController, ContactsControllerOptions } from "./controllers";
+import { ContactsCollection, Contact } from "./models";
+import minimist from "minimist";
 
 function parseaParams(argv): ContactsControllerOptions {
+
+  const resultado = minimist(argv);
   // parsear el argv usando https://www.npmjs.com/package/minimist
   return {
-    action: null,
-    params: null,
+    action: resultado.action,
+    params: JSON.parse(resultado.params),
   };
 }
 
-function main() {}
+function main() {
+  
+  const controller = new ContactsController();
+  const params = parseaParams(process.argv.slice(2));
+  console.log(controller.processOptions(params));
+
+}
 
 main();
